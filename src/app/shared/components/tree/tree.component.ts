37ec8e6b9f9030af0cd84a4ce6996849a68dc224
celmoussaoui <C.ElMoussaoui@student.tudelft.nl>
2021-11-15 16:34:52 +0100

    Fix checkpoint level bug

--- src/app/shared/components/tree/tree.component.ts
+++ src/app/shared/components/tree/tree.component.ts
@@ -12,6 +12,8 @@ export class TreeComponent {
   @Input() reports: any[] = [];
   tree: any[] = []
   treeId: string = Math.random().toString(36).substring(7);
+  parentMap: any[] = []
+
 
   constructor() {}
 
@@ -57,6 +59,18 @@ export class TreeComponent {
     }
   }
 
+  findParent(currentNode: any, potentialParent: any): any {
+    // If the level difference is only 1, then the potential parent is the actual parent
+    if (currentNode.level - 1 == potentialParent.level) {
+      this.parentMap.push({id: currentNode.id, parent: potentialParent})
+      potentialParent.nodes.push(currentNode)
+      return currentNode;
+    }
+
+    let newPotentialParent = this.parentMap.find(x => x.id == potentialParent.id).parent;
+    return this.findParent(currentNode, newPotentialParent)
+  }
+
   /**
    * Handle change in the tree for the tree view
    * @param reports - the reports to be displayed
@@ -68,8 +82,11 @@ export class TreeComponent {
     this.tree = [];
     let id = 0;
 
+
     // For each item that has been selected show the node and its children
     for (let report of this.reports) {
+      this.parentMap = []
+      console.log(report)
       let rootNode = {
         text: report.name,
         ladybug: report,
@@ -78,34 +95,43 @@ export class TreeComponent {
         nodes: []
       }
 
-      // Keep track of the previous node (which could be the parent)
-      let previousNode: any = {};
-
-      // For each of the child nodes add it to the parent
+      let previousNode: any = rootNode;
       for (let checkpoint of report.checkpoints) {
         let node = {
           text: checkpoint.name,
           ladybug: checkpoint,
           root: false,
           id: id++,
-          level: checkpoint.level
+          level: checkpoint.level,
+          backColor: checkpoint.level % 2 == 0 ? 'lightgrey' : 'white',
+          nodes: []
         }
 
-        // If the previous node is its parent, push to the parent
-        if (checkpoint.index > 0 && report.checkpoints[checkpoint.index - 1].level < checkpoint.level) {
-          // If it doesnt have children yet, make sure it can have
-          if (previousNode.nodes === undefined) {
-            previousNode.nodes = [];
-          }
-          previousNode.nodes.push(node);
-        } else {
-          // Push it to the root
+        // If it is the first one, the parent is the root
+        if (node.level == 0) {
+          this.parentMap.push({id: node.id, parent: rootNode})
           // @ts-ignore
           rootNode.nodes.push(node)
-          previousNode = node;
+
+          // If the level is higher, then the previous node was its parent
+        } else if (node.level > previousNode.level) {
+          this.parentMap.push({id: node.id, parent: previousNode})
+          previousNode.nodes.push(node)
+
+          // If the level is lower, then the previous node is a (grand)child of this node's sibling
+        } else if (node.level < previousNode.level) {
+          node = this.findParent(node, previousNode)
+
+          // Else the level is equal, meaning the previous node is its sibling
+        } else {
+          let newParent = this.parentMap.find(x => x.id == previousNode.id).parent;
+          this.parentMap.push({id: node.id, parent: newParent})
+          newParent.nodes.push(node)
         }
 
+        previousNode = node
       }
+
       this.tree.push(rootNode)
     }
 
