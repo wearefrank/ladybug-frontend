a8ebafa806b60f68f3e04edabdf4fd1b4fafd219
Martijn Dirkse <martijn@integrationpartners.nl>
2022-01-28 18:02:33 +0100

    Finish collapse and expand test

--- cypress/integration/debug_aboutOpenedReports.spec.js
+++ cypress/integration/debug_aboutOpenedReports.spec.js
@@ -1,3 +1,6 @@
+const REGEX_START_ICON = new RegExp('.*startpoint-(even|odd)\\.gif');
+const REGEX_END_ICON = new RegExp('.*endpoint-(even|odd)\\.gif');
+
 describe('About opened reports', function() {
   beforeEach(() => {
     cy.createReport();
@@ -37,31 +40,57 @@ describe('About opened reports', function() {
   it('Expand and collapse', function() {
     cy.get('button[id="Open AllButton"]').click();    
     cy.get('div.treeview > ul > li').should('have.length', 6);
-    cy.get('div.treeview > ul > li:contains(name)').should('have.length', 3);
-    cy.get('div.treeview > ul > li:contains(name)').within(($lines) => {
-      expect($lines.eq(0).children().eq(0)).to.have.prop('nodeName', 'SPAN');
-      expect($lines.eq(0).children().eq(0)).to.have.class('expand-icon');
-      expect($lines.eq(0).children().eq(1)).to.have.prop('nodeName', 'SPAN');
-      expect($lines.eq(0).children().eq(1)).to.have.class('node-icon');
-      expect($lines.eq(0).children('img')).to.have.length(0);
-      expect($lines.eq(1).children().eq(0)).to.have.prop('nodeName', 'SPAN');
-      expect($lines.eq(1).children().eq(1)).to.have.prop('nodeName', 'SPAN');
-      expect($lines.eq(1).children().eq(2)).to.have.prop('nodeName', 'SPAN');
-      expect($lines.eq(1).children().eq(3)).to.have.prop('nodeName', 'IMG');
-      expect($lines.eq(1).children().eq(0)).to.have.class('indent');
-      expect($lines.eq(1).children().eq(1)).to.have.class('expand-icon');
-      expect($lines.eq(1).children().eq(2)).to.have.class('node-icon');
-      expect($lines.eq(1).children().eq(3).attr('src')).contains('startpoint-even.gif');
-      expect($lines.eq(2).children().eq(0)).to.have.prop('nodeName', 'SPAN');
-      expect($lines.eq(2).children().eq(1)).to.have.prop('nodeName', 'SPAN');
-      expect($lines.eq(2).children().eq(2)).to.have.prop('nodeName', 'SPAN');
-      expect($lines.eq(2).children().eq(3)).to.have.prop('nodeName', 'SPAN');
-      expect($lines.eq(2).children().eq(4)).to.have.prop('nodeName', 'IMG');
-      expect($lines.eq(2).children().eq(0)).to.have.class('indent');
-      expect($lines.eq(2).children().eq(1)).to.have.class('indent');
-      expect($lines.eq(2).children().eq(2)).to.have.class('glyphicon');
-      expect($lines.eq(2).children().eq(3)).to.have.class('node-icon');
-      expect($lines.eq(2).children().eq(4).attr('src')).contains('endpoint-odd.gif');
-    });
+    cy.get('div.treeview > ul > li:contains(name)').within(linesFormExpandedNode);
+    cy.get('div.treeview > ul > li:contains(otherName)').within(linesFormExpandedNode);
+    cy.get('button[id="Collapse allButton"]').click();
+    cy.get('div.treeview > ul > li').should('have.length', 2);
+    cy.get('div.treeview > ul > li:contains(name)').within(linesFormCollapsedNode);
+    cy.get('div.treeview > ul > li:contains(otherName)').within(linesFormCollapsedNode);
+    cy.get('button[id="Expand AllButton"]').click();
+    cy.get('div.treeview > ul > li').should('have.length', 6);
+    cy.get('div.treeview > ul > li:contains(name)').within(linesFormExpandedNode);
+    cy.get('div.treeview > ul > li:contains(otherName)').within(linesFormExpandedNode);   
   });
 });
+
+function linesFormExpandedNode($lines) {
+  expect($lines).to.have.length(3);
+  expect($lines.eq(0).children()).to.have.length(2);
+  expect($lines.eq(0).children().eq(0)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(0).children().eq(0)).to.have.class('expand-icon');
+  expect($lines.eq(0).children().eq(0)).to.have.class('fa-minus');
+  expect($lines.eq(0).children().eq(1)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(0).children().eq(1)).to.have.class('node-icon');
+  expect($lines.eq(0).children('img')).to.have.length(0);
+  expect($lines.eq(1).children()).to.have.length(4);
+  expect($lines.eq(1).children().eq(0)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(1).children().eq(1)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(1).children().eq(2)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(1).children().eq(3)).to.have.prop('nodeName', 'IMG');
+  expect($lines.eq(1).children().eq(0)).to.have.class('indent');
+  expect($lines.eq(1).children().eq(1)).to.have.class('expand-icon');
+  expect($lines.eq(1).children().eq(1)).to.have.class('fa-minus');
+  expect($lines.eq(1).children().eq(2)).to.have.class('node-icon');
+  expect($lines.eq(1).children().eq(3).attr('src')).to.match(REGEX_START_ICON);
+  expect($lines.eq(2).children()).to.have.length(5);
+  expect($lines.eq(2).children().eq(0)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(2).children().eq(1)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(2).children().eq(2)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(2).children().eq(3)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(2).children().eq(4)).to.have.prop('nodeName', 'IMG');
+  expect($lines.eq(2).children().eq(0)).to.have.class('indent');
+  expect($lines.eq(2).children().eq(1)).to.have.class('indent');
+  expect($lines.eq(2).children().eq(2)).to.have.class('glyphicon');
+  expect($lines.eq(2).children().eq(3)).to.have.class('node-icon');
+  expect($lines.eq(2).children().eq(4).attr('src')).to.match(REGEX_END_ICON);
+};
+
+function linesFormCollapsedNode($lines) {
+  expect($lines).to.have.length(1);
+  expect($lines.eq(0).children()).to.have.length(2);
+  expect($lines.eq(0).children().eq(0)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(0).children().eq(1)).to.have.prop('nodeName', 'SPAN');
+  expect($lines.eq(0).children().eq(0)).to.have.class('expand-icon');
+  expect($lines.eq(0).children().eq(0)).to.have.class('fa-plus');
+  expect($lines.eq(0).children().eq(1)).to.have.class('node-icon');
+}
