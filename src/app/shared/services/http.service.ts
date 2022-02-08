c84edde5726154a62f978b3151e44e180fc621dd
celmoussaoui <C.ElMoussaoui@student.tudelft.nl>
2022-02-08 15:29:42 +0100

    Show more elaborate toast message

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -18,9 +18,10 @@ export class HttpService {
     this.toastComponent = toastComponent;
   }
 
-  handleError(message: string) {
+  handleError() {
     return (error: any): Observable<any> => {
-      this.toastComponent.addAlert({ type: 'danger', message: message });
+      const errorMessages = error.error.split('::');
+      this.toastComponent.addAlert({ type: 'danger', message: errorMessages[0], detailed: errorMessages[1] });
       return of(error);
     };
   }
@@ -39,7 +40,7 @@ export class HttpService {
     return this.http
       .get('api/report/latest/debugStorage/' + amount)
       .pipe(tap(() => this.handleSuccess('Latest' + amount + 'reports opened!')))
-      .pipe(catchError(this.handleError('Could not open latest reports!')));
+      .pipe(catchError(this.handleError()));
   }
 
   getTestReports(): Observable<any> {
@@ -48,9 +49,9 @@ export class HttpService {
 
   getReport(reportId: string): Observable<any> {
     return this.http
-      .get<any>(this.REPORT_DEBUG_STORAGE + reportId)
+      .get<any>(this.REPORT_DEBUG_STORAGE + '-1')
       .pipe(tap(() => this.handleSuccess('Report opened!')))
-      .pipe(catchError(this.handleError('Could not retrieve data for report!')));
+      .pipe(catchError(this.handleError()));
   }
 
   getMonacoCode(reportId: string): Observable<any> {
@@ -59,21 +60,21 @@ export class HttpService {
       : 'false';
     return this.http
       .get<any>(this.REPORT_DEBUG_STORAGE + reportId + '/?xml=true&globalTransformer=' + xmlTransformationEnabled)
-      .pipe(catchError(this.handleError('Could not retrieve monaco code!')));
+      .pipe(catchError(this.handleError()));
   }
 
   postReport(reportId: string, report: any): Observable<void> {
     return this.http
       .post(this.REPORT_DEBUG_STORAGE + reportId, report)
       .pipe(tap(() => this.handleSuccess('Report updated!')))
-      .pipe(catchError(this.handleError('Could not retrieve data for report!')));
+      .pipe(catchError(this.handleError()));
   }
 
   copyReport(data: any): Observable<void> {
     return this.http
       .put('api/report/store/testStorage', data)
       .pipe(tap(() => this.handleSuccess('Report copied!')))
-      .pipe(catchError(this.handleError('Could not copy report into test tab!')));
+      .pipe(catchError(this.handleError()));
   }
 
   uploadReport(formData: FormData): Observable<any> {
@@ -82,7 +83,7 @@ export class HttpService {
         headers: { 'Content-Type': 'multipart/form-data' },
       })
       .pipe(tap(() => this.handleSuccess('Report uploaded!')))
-      .pipe(catchError(this.handleError('Could not retrieve uploaded report!')));
+      .pipe(catchError(this.handleError()));
   }
 
   uploadReportToStorage(formData: FormData): Observable<any> {
@@ -91,35 +92,33 @@ export class HttpService {
         headers: { 'Content-Type': 'multipart/form-data' },
       })
       .pipe(tap(() => this.handleSuccess('Report uploaded to storage!')))
-      .pipe(catchError(this.handleError('Could not upload report to storage!')));
+      .pipe(catchError(this.handleError()));
   }
 
   postSettings(settings: any): Observable<void> {
     return this.http
       .post('api/testtool', settings)
       .pipe(tap(() => this.handleSuccess('Settings saved!')))
-      .pipe(catchError(this.handleError('Could not save settings!')));
+      .pipe(catchError(this.handleError()));
   }
 
   postTransformation(transformation: any): Observable<void> {
     return this.http
       .post('api/testtool/transformation', { transformation: transformation })
       .pipe(tap(() => this.handleSuccess('Transformation saved!')))
-      .pipe(catchError(this.handleError('Could not save transformation!')));
+      .pipe(catchError(this.handleError()));
   }
 
   getTransformation(): Observable<any> {
-    return this.http
-      .get<any>('api/testtool/transformation')
-      .pipe(catchError(this.handleError('Could not retrieve transformation!')));
+    return this.http.get<any>('api/testtool/transformation').pipe(catchError(this.handleError()));
   }
 
   getSettings(): Observable<any> {
-    return this.http.get<any>('api/testtool').pipe(catchError(this.handleError('Could not retrieve settings!')));
+    return this.http.get<any>('api/testtool').pipe(catchError(this.handleError()));
   }
 
   reset(): Observable<void> {
-    return this.http.post<any>('api/runner/reset', {}).pipe(catchError(this.handleError('Could not reset runner!')));
+    return this.http.post<any>('api/runner/reset', {}).pipe(catchError(this.handleError()));
   }
 
   runReport(report: any): Observable<void> {
@@ -128,7 +127,7 @@ export class HttpService {
         headers: this.headers,
         observe: 'response',
       })
-      .pipe(catchError(this.handleError('Could not correctly run report(s)!')));
+      .pipe(catchError(this.handleError()));
   }
 
   runDisplayReport(reportId: string): Observable<any> {
@@ -137,21 +136,21 @@ export class HttpService {
         headers: this.headers,
         observe: 'response',
       })
-      .pipe(catchError(this.handleError('Could not correctly run display report!')));
+      .pipe(catchError(this.handleError()));
   }
 
   queryResults(): Observable<any> {
     return this.http
       .get('api/runner/result/debugStorage', { headers: this.headers })
       .pipe(tap(() => this.handleSuccess('Test run(s) completed!')))
-      .pipe(catchError(this.handleError('Could not retrieve runner results!')));
+      .pipe(catchError(this.handleError()));
   }
 
   deleteReport(reportId: string): Observable<void> {
     return this.http
       .delete('api/report/testStorage/' + reportId)
       .pipe(tap(() => this.handleSuccess('Report deleted!')))
-      .pipe(catchError(this.handleError('Could not delete report!')));
+      .pipe(catchError(this.handleError()));
   }
 
   replaceReport(reportId: string): Observable<void> {
@@ -159,6 +158,6 @@ export class HttpService {
       .put('api/runner/replace/testStorage/' + reportId, {
         headers: this.headers,
       })
-      .pipe(catchError(this.handleError('Could not update report!')));
+      .pipe(catchError(this.handleError()));
   }
 }
