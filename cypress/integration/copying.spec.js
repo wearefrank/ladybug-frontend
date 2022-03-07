c0818c7827ae9c9a562e0fac8d3b160c1aff99aa
Martijn Dirkse <martijn@integrationpartners.nl>
2022-03-07 16:38:50 +0100

    Work in progress

--- cypress/integration/copying.spec.js
+++ cypress/integration/copying.spec.js
@@ -8,15 +8,23 @@ describe('Tests about copying', function() {
     cy.get('#testTab').click();
     cy.get('#metadataTable tbody').find('tr').should('have.length', 0);
     cy.createReport();
+    cy.createOtherReport();
     cy.get('#debugTab').click();
     cy.get('#metadataTable tbody').find('tr').should('have.length', 0);
-    cy.get('#RefreshButton').click();
-    cy.get('#metadataTable tbody').find('tr').should('have.length', 1);
+    cy.get('.row #RefreshButton').click();
+    cy.get('#metadataTable tbody').find('tr').should('have.length', 2);
     cy.get('button[id="OpenAllButton"]').click();
-    cy.get('div.treeview > ul > li').should('have.length', 3);
+    cy.get('div.treeview > ul > li').should('have.length', 6);
     // :eq expects a zero-based index
-    cy.get('div.treeview > ul > li:eq(1)').click();
-    // TODO: Cannot continue until issue https://github.com/ibissource/ladybug-frontend/issues/70
-    // has been fixed
+    cy.get('div.treeview > ul > li:not(:contains(other)):eq(1)').click();
+    cy.get('button#CopyButton').click();
+    cy.get('li#testTab').click();
+    cy.get('button#RefreshButton').click();
+    cy.get('tbody#testReports').find('tr').should('have.length', 1).within(function(testReport) {
+      cy.wrap(testReport).contains('/name').should('have.length', 1);
+    });
+    cy.get('#debugTab').click();
+    cy.get('#metadataTable tbody').find('tr').should('have.length', 2);
+    cy.get('div.treeview > ul > li').should('have.length', 6);
   });
 });
