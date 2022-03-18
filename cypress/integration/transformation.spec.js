a3cf100c562903b173bafafd28502a098886eb74
Martijn Dirkse <martijn@integrationpartners.nl>
2022-03-18 12:54:48 +0100

    Correct because transformation is applied on server

--- cypress/integration/transformation.spec.js
+++ cypress/integration/transformation.spec.js
@@ -1,36 +1,29 @@
 describe('Report transformation', function() {
   afterEach(function() {
     cy.clearDebugStore();
-    /*
     cy.get('#SettingsButton').click();
     // Factory reset in settings dialog. Resets
     // transformation to factory value.
-    cy.get('.model-footer :eq(0)').click();
+    cy.get('button[title ^= "Reset to factory settings"]').click();
     cy.get('button[type=submit]').click();
-    */
   });
 
   it('Update transformation', function() {
     cy.visit('');
-    /*
-    cy.createOtherReport();
-    cy.get('.table-responsive tbody').find('tr').should('have.length', 1).click();
-    cy.get('div.treeview > ul > li').should('have.length', 3);
-    cy.get('div.treeview > ul > li:eq(0)').click();
-    cy.get('#monacoEditor').contains('Name="otherName"');
-    */
     cy.get('#SettingsButton').click();
     cy.get('textarea[formcontrolname=transformation]').type('{selectAll}');
     cy.get('textarea[formcontrolname=transformation]').type('{del}');
     cy.get('textarea[formcontrolname=transformation]').within((textArea) => {
       cy.fixture('ignoreName.xslt').then((newText) => cy.wrap(textArea).type(newText));
     });
+    cy.get('input[type=checkbox][formcontrolname=transformationEnabled]').check();
     cy.get('button[type=submit]').click();
-    cy.get('#RefreshButton').click();
     cy.createOtherReport();
+    cy.get('#RefreshButton').click();
     cy.get('.table-responsive tbody').find('tr').should('have.length', 1).click();
     cy.get('div.treeview > ul > li').should('have.length', 3);
+    // We test that the top node was not selected before.
     cy.get('div.treeview > ul > li:eq(0)').click();
-    cy.get('#monacoEditor').contains('Name="IGNORE"');
+    cy.get('#monacoEditor').contains('Name="IGNORED"');
   });
 });
