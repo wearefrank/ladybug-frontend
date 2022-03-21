1b084d54837bbf426ba5d142ba57d179568b58ef
Martijn Dirkse <martijn@integrationpartners.nl>
2022-03-21 16:24:53 +0100

    Try to fix flaky test

--- cypress/integration/debug_reportGenerator.spec.js
+++ cypress/integration/debug_reportGenerator.spec.js
@@ -15,6 +15,8 @@ describe('Report generator', function() {
     cy.get('#SettingsButton').click();
     cy.get('select[formcontrolname=generatorEnabled]').select('Disabled').should('have.value', 'Disabled');
     cy.get('button[title="Save changes"').click();
+    // Give the server time to process disabling the report generator
+    cy.wait(5000);
     cy.createOtherReport();
     // If we do not wait here, we do not test properly that no report is created.
     // Without waiting, the test could succeed because we would count the number of reports
