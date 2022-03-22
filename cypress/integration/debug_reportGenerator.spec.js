df5b27e3c66486f47aef7456f26a9049f6488304
Martijn Dirkse <martijn@integrationpartners.nl>
2022-03-22 12:47:50 +0100

    Fix flaky test debug_reportGenerator

--- cypress/integration/debug_reportGenerator.spec.js
+++ cypress/integration/debug_reportGenerator.spec.js
@@ -2,6 +2,7 @@ describe('Report generator', function() {
   afterEach(function() {
     cy.clearDebugStore();
     cy.get('#SettingsButton').click();
+    cy.get('[role=dialog]').should('be.visible', {timeout: 10000});
     cy.get('select[formcontrolname=generatorEnabled]').select('Enabled').should('have.value', 'Enabled');
     cy.get('button[title="Save changes"').click();
   });
@@ -13,10 +14,11 @@ describe('Report generator', function() {
     cy.get('#RefreshButton').click();
     cy.get('.table-responsive tbody').find('tr').should('have.length', 1);
     cy.get('#SettingsButton').click();
+    // I want to see whether this check is correct. TODO: Maybe remove.
+    cy.get('[role=dialog]').should('be.visible', {timeout: 10000});
     cy.get('select[formcontrolname=generatorEnabled]').select('Disabled').should('have.value', 'Disabled');
     cy.get('button[title="Save changes"').click();
-    // Give the server time to process disabling the report generator
-    cy.wait(5000);
+    cy.get('.alert').contains('Settings saved');
     cy.createOtherReport();
     // If we do not wait here, we do not test properly that no report is created.
     // Without waiting, the test could succeed because we would count the number of reports
@@ -25,9 +27,11 @@ describe('Report generator', function() {
     cy.get('#RefreshButton').click();
     cy.wait(5000);
     cy.get('.table-responsive tbody').find('tr').should('have.length', 1);
+    cy.get('.alert').should('not.exist');
     cy.get('#SettingsButton').click();
     cy.get('select[formcontrolname=generatorEnabled]').select('Enabled').should('have.value', 'Enabled');
     cy.get('button[title="Save changes"').click();
+    cy.get('.alert').contains('Settings saved');
     cy.createOtherReport();
     cy.get('#RefreshButton').click();
     cy.get('.table-responsive tbody').find('tr').should('have.length', 2);
