0db823bb8955c0d30ff695d3fb90e476ce8d22fc
Martijn Dirkse <mhdirkse@live.nl>
2022-04-04 11:13:08 +0200

    Download test upload debug (#126)
    * Work in progress
    
    * Finish the test
    
    * Try to fix in CI/CD
    
    * Fix bug
    
    * Undo change that is not necessary
    
    Co-authored-by: Martijn Dirkse <martijn@integrationpartners.nl>

--- cypress/integration/testtab.spec.js
+++ cypress/integration/testtab.spec.js
@@ -3,32 +3,32 @@ describe('About the Test tab', function() {
     cy.createReport();
     cy.createOtherReport();
     cy.visit('');
+    copyTheReportsToTestTab();
   });
 
   afterEach(() => {
     cy.clearDebugStore();
+    cy.get('li#debugTab').click();
+    cy.get('button[id="CloseAllButton"]').click();
+    cy.get('div.treeview > ul > li').should('have.length', 0);
     cy.get('li#testTab').click();
+    // Give UI time to build up the test tab.
+    cy.wait(1000);
     cy.get('#SelectAllButton').click();
     cy.get('#DeleteSelectedButton').click();
+    cy.get('#testReports tr', {timeout: 10000}).should('have.length', 0);
     cy.get('li#debugTab').click();
+    const downloadsFolder = Cypress.config('downloadsFolder');
+    cy.task('deleteDownloads', {downloadsPath: downloadsFolder, fileSep: Cypress.env('FILESEP')});
   });
 
   it('Test deleting a report', function() {
-    copyTheReportsToTestTab();
     cy.get('li#testTab').click();
     cy.get('#testReports').find('tr').should('have.length', 2).within(function($reports) {
       cy.wrap($reports).contains('/name').should('have.length', 1);
       cy.wrap($reports).contains('/otherName').should('have.length', 1);
-      cy.wrap($reports).each(function($reportRow) {
-        cy.log('Considering next report');
-        const reportName = $reportRow.find('td').eq(3).text();
-        cy.log('Name of report is: ' + reportName);
-        if(reportName.includes('name')) {
-          cy.log('Found checkbox of report with name name, checking it');
-          cy.wrap($reportRow).find('[type=checkbox]').check();
-        };
-      });
     });
+    testTabSelectReportNamed('name');
     cy.get('#DeleteSelectedButton').click();
     cy.get('#testReports').find('tr').should('have.length', 1).within(function($reports) {
       cy.wrap($reports).contains('/otherName').should('have.length', 1);
@@ -36,28 +36,25 @@ describe('About the Test tab', function() {
   });
 
   it('Test select all by deleting', function() {
-    copyTheReportsToTestTab();
     cy.get('li#testTab').click();
     cy.get('#testReports').find('tr').should('have.length', 2).within(function($reports) {
       cy.wrap($reports).contains('/name').should('have.length', 1);
       cy.wrap($reports).contains('/otherName').should('have.length', 1);
     });
     cy.get('#SelectAllButton').click();
+    checkTestTabTwoReportsSelected();
     cy.get('#DeleteSelectedButton').click();
     cy.get('#testReports').find('tr').should('have.length', 0);
   });
 
   it('Test deselect all', function() {
-    copyTheReportsToTestTab();
     cy.get('li#testTab').click();
     cy.get('#testReports').find('tr').should('have.length', 2).within(function($reports) {
       cy.wrap($reports).contains('/name').should('have.length', 1);
       cy.wrap($reports).contains('/otherName').should('have.length', 1);
     });
     cy.get('#SelectAllButton').click();
-    cy.get('#testReports tr [type=checkbox]').should('have.length', 2).each(($checkbox) => {
-      cy.wrap($checkbox).should('be.checked');
-    });
+    checkTestTabTwoReportsSelected();
     cy.get('#DeselectAllButton').click();
     cy.get('#DeleteSelectedButton').click();
     cy.wait(5000);
@@ -65,7 +62,48 @@ describe('About the Test tab', function() {
       cy.wrap($reports).contains('/name').should('have.length', 1);
       cy.wrap($reports).contains('/otherName').should('have.length', 1);
     });
-  })
+  });
+
+  it('Download from tab test, upload to tab debug', function() {
+    const downloadsFolder = Cypress.config('downloadsFolder');
+    cy.get('li#testTab').click();
+    cy.get('#testReports').find('tr').should('have.length', 2).within(function($reports) {
+      cy.wrap($reports).contains('/name').should('have.length', 1);
+      cy.wrap($reports).contains('/otherName').should('have.length', 1);
+    });
+    testTabSelectReportNamed('name');
+    cy.task('downloads', downloadsFolder).then(filesBefore => {
+      cy.log('Before download, downloads folder contains files: ' + filesBefore.toString());
+      cy.get('#DownloadBinaryButton').click();
+      cy.waitForNumFiles(downloadsFolder, filesBefore.length + 1);
+      cy.task('downloads', downloadsFolder).then(filesAfter => {
+        cy.log('After download, downloads folder contains files: ' + filesAfter.toString());
+        const newFile = filesAfter.filter(file => !filesBefore.includes(file))[0];
+        expect(newFile).to.contain('name.ttr');
+        cy.readFile(cy.functions.downloadPath(newFile), 'binary', {timeout: 15000})
+        .should(buffer => expect(buffer.length).to.be.gt(10)).then(buffer => {
+          cy.log(`Number of read bytes: ${buffer.length}`);
+        });
+        cy.get('li#debugTab').click();
+        // Wait for the front-end to complete showing the page
+        cy.get('div.treeview > ul > li').should('have.length', 6);
+        cy.readFile(cy.functions.downloadPath(newFile), 'binary')
+        .then((rawContent) => {
+          console.log(`Have content of uploaded file, length ${rawContent.length}`);
+          return Cypress.Blob.binaryStringToBlob(rawContent);
+        })
+        .then(fileContent => {
+          console.log(`Have transformed content length ${fileContent.length}`);
+          cy.get('input#uploadFileTable').attachFile({
+            fileContent,
+            fileName: newFile
+          });
+        });
+        cy.get('div.treeview > ul > li').should('have.length', 9);
+        cy.get('div.treeview > ul > li:contains(name)').should('have.length', 6);
+      });
+    });
+  });
 });
 
 function copyTheReportsToTestTab() {
@@ -80,4 +118,24 @@ function copyTheReportsToTestTab() {
   cy.get('button#CopyButton').click();
   cy.get('div.treeview > ul > li:contains(otherName)').first().selectIfNotSelected();
   cy.get('button#CopyButton').click();
+}
+
+function checkTestTabTwoReportsSelected() {
+  cy.get('#testReports tr [type=checkbox]').should('have.length', 2).each(($checkbox) => {
+    cy.wrap($checkbox).should('be.checked');
+  });
+}
+
+function testTabSelectReportNamed(nameToSelect) {
+  cy.get('#testReports').find('tr').each(function($reportRow) {
+    cy.log('Considering next report');
+    const reportName = $reportRow.find('td').eq(3).text();
+    cy.log('Name of report is: ' + reportName);
+    if(reportName.includes(nameToSelect)) {
+      cy.log('Found checkbox of report with name name, checking it');
+      cy.wrap($reportRow).find('[type=checkbox]').check();
+    };
+    cy.get('#testReports tr [type=checkbox]:checked').should('have.length', 1);
+    cy.wait(1000);
+  });
 }
