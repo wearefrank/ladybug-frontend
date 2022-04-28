a5c218c4ac1fb93bfb3dc91b6fc49ae9354f6bd6
Martijn Dirkse <martijn@integrationpartners.nl>
2022-04-28 18:57:09 +0200

    ../cypress/integration/testtab_edit.spec.js

--- cypress/integration/testtab_edit.spec.js
+++ cypress/integration/testtab_edit.spec.js
@@ -1,7 +1,12 @@
+const { ChildProcess } = require("child_process");
+
 describe('Edit tests', function() {
   afterEach(function() {
     cy.clearDebugStore();
-    cy.get('li#debugTab').click();
+    cy.get('li#debugTab a').click();
+    cy.get('li#debugTab a:eq(0)').should('have.class', 'active');
+    // Wait for debug tab to be rendered
+    cy.wait(1000);
     cy.get('button[id="CloseAllButton"]').click();
     cy.get('div.treeview > ul > li').should('have.length', 0);
     cy.get('li#testTab').click();
@@ -14,34 +19,87 @@ describe('Edit tests', function() {
   });
 
   it('Edit report in test tab', function() {
-    cy.createReport();
-    cy.visit('');
-    cy.get('button[id="OpenAllButton"]').click();
-    cy.get('div.treeview > ul > li').should('have.length', 3);
-    cy.get('div.treeview > ul > li:contains(name)').first().selectIfNotSelected();
-    cy.get('button#CopyButton').click();
-    cy.get('li#testTab').click();
-    cy.get('#OpenreportButton').click();
-    cy.get('.nav-tabs ul li:nth-child:nth-child(4)').find('a.active').should('include.text', 'name');
-    // TODO: These two lines are a work-around for issue https://github.com/ibissource/ladybug-frontend/issues/150
+    prepareEdit();
+    // TODO: These four lines are a work-around for issue https://github.com/ibissource/ladybug-frontend/issues/150
     // These lines should be removed if that issue has been fixed.
-    cy.get('.treeview ul li:nth-child(2)').click().should('not.have.class', 'node-selected');
-    cy.get('.treeview ul li:nth-child(2)').click().should('have.class', 'node-selected');
     //
-    // TODO: Test that an error is shown if you try to edit before pressing the edit button.
-    cy.get('.displayButtons #EditButton').click();
+    // Doing the "should" right after the "click" does not work. Then you
+    // get an error that the element is detached from the DOM.
+    // It seems that another node is created when the "click" is handled.
+    cy.get('.treeview ul li:nth-child(2)').click()
+    cy.get('.treeview ul li:nth-child(2)').should('not.have.class', 'node-selected');
+    cy.get('.treeview ul li:nth-child(2)').click()
+    cy.get('.treeview ul li:nth-child(2)').should('have.class', 'node-selected');
+    cy.wait(1000);
+    cy.get('#EditButton').click();
+    cy.wait(1000);
     // According to https://stackoverflow.com/questions/56617522/testing-monaco-editor-with-cypress
     cy.get('#monacoEditor').click().focused().type('{ctrl}a').type('Hello Original World!');
-    // TODO: Test that the text before and the text after the change is shown.
+    cy.get('#SaveButton').click()
+    cy.get('.modal-title').should('include.text', 'Are you sure');
+    cy.get('.col:not(.text-right)').contains('Hello World!');
+    cy.get('.col.text-right').contains('Hello Original World!');
     cy.get('button:contains(Yes)').click();
-
-    cy.get('.treeview ul li:nth-child(3)').click().should('have.class', 'node-selected');
-    cy.get('.displayButtons #EditButton').click();
+    cy.get('.treeview ul li:nth-child(3)').click()
+    cy.get('.treeview ul li:nth-child(3)').should('have.class', 'node-selected');
+    cy.wait(1000);
+    cy.get('#EditButton').click();
+    cy.wait(1000);
     cy.get('#monacoEditor').click().focused().type('{ctrl}a').type('Goodbye Original World!');
+    cy.get('#SaveButton').click()
     cy.get('button:contains(Yes)').click();
-
     cy.get('li#testTab').click();
     cy.get('#RunreportButton').click();
-    cy.get('span:contains(0/1 stubbed').should('have.property', 'style', 'color: green;');
+    const GREEN = 'rgb(33, 37, 41)'
+    cy.get('span:contains(0/1 stubbed)').should('have.css', 'color', GREEN);
+  });
+
+  it('Editing without pressing Edit produces error', function() {
+    prepareEdit();
+    // TODO: These four lines are a work-around for issue https://github.com/ibissource/ladybug-frontend/issues/150
+    // These lines should be removed if that issue has been fixed.
+    cy.get('.treeview ul li:nth-child(2)').click()
+    cy.get('.treeview ul li:nth-child(2)').should('not.have.class', 'node-selected');
+    cy.get('.treeview ul li:nth-child(2)').click()
+    cy.get('.treeview ul li:nth-child(2)').should('have.class', 'node-selected');
+    // Do not press Edit button
+    cy.get('#SaveButton').should('have.length', 0);
+    cy.get('#monacoEditor').click().type('x');
+    cy.contains('Cannot edit');
+    cy.get('.message').should('have.length', 0);
+  });
+
+  it('When saving edit cancelled then original text kept', function() {
+    prepareEdit();
+    // TODO: These four lines are a work-around for issue https://github.com/ibissource/ladybug-frontend/issues/150
+    // These lines should be removed if that issue has been fixed.
+    cy.get('.treeview ul li:nth-child(2)').click()
+    cy.get('.treeview ul li:nth-child(2)').should('not.have.class', 'node-selected');
+    cy.get('.treeview ul li:nth-child(2)').click()
+    cy.get('.treeview ul li:nth-child(2)').should('have.class', 'node-selected');
+    cy.wait(1000);
+    cy.get('#EditButton').click();
+    cy.wait(1000);
+    // According to https://stackoverflow.com/questions/56617522/testing-monaco-editor-with-cypress
+    cy.get('#monacoEditor').click().focused().type('{ctrl}a').type('Hello Original World!');
+    cy.get('#SaveButton').click()
+    cy.get('.modal-title').should('include.text', 'Are you sure');
+    cy.contains('Hello Original World!');
+    cy.get('button:contains(No)').click();
+    cy.contains('Hello World!');
   });
 });
+
+function prepareEdit() {
+  cy.createReport();
+  cy.visit('');
+  cy.get('button[id="OpenAllButton"]').click();
+  cy.get('div.treeview > ul > li').should('have.length', 3);
+  cy.get('button#CopyButton').click();
+  cy.get('li#testTab').click();
+  cy.get('#testReports tr').should('have.length', 1);
+  cy.get('#OpenreportButton').click();
+  cy.get('ul.nav-tabs li:nth-child(4)').find('a.active').should('include.text', 'name');
+  // Wait until the tab has been rendered
+  cy.get('.treeview ul li').should('have.length', 3);
+}
