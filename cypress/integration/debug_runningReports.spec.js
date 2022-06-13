6db718902f1189f6e43992cd959da4a83f93af61
Martijn Dirkse <martijn@integrationpartners.nl>
2022-06-13 10:45:28 +0200

    Fix issues with this test

--- cypress/integration/debug_runningReports.spec.js
+++ cypress/integration/debug_runningReports.spec.js
@@ -3,7 +3,7 @@ describe('Test running reports', function() {
     cy.clearDebugStore();
   });
 
-  it('If no running reports than no number or running reports is zero', function() {
+  it('If no running reports then number of running reports is zero', function() {
     cy.visit('');
     cy.contains('Reports in progress: 0');
   })
@@ -26,9 +26,12 @@ describe('Test running reports', function() {
       cy.wait(5000);
       cy.get('#metadataTable tbody').find('tr').should('have.length', 0);
       cy.get('#SettingsButton').should('be.visible').click();
+      cy.get('#openInProgressNo').type('{backspace}');
+      cy.get('#openInProgressNo').should('have.text', '');
       cy.get('#openInProgressNo').type('1');
-      cy.get('#buttonOpenInProgressNo').click();
+      cy.get('#openReportInProgressButton').click();
       cy.get('#debugSettingsModalClose').click();
+      // TODO: Shouldn't there be a toast that the report in progress has been opened?
       cy.get('div.treeview > ul > li').should('have.length', 3);
       cy.get('div.treeview > ul > li:nth-child(1)').should('have.text', 'name');
       cy.get('div.treeview > ul > li:nth-child(2)').should('have.text', 'name');
@@ -45,16 +48,18 @@ describe('Test running reports', function() {
         cy.wrap($node).find('span:nth-child(2).indent');
         cy.wrap($node).find('span:nth-child(3).glyphicon');
         cy.wrap($node).find('span:nth-child(4).node-icon');
-        cy.wrap($node).find('img:nth-child(5)').invoke('attr', 'src').should('eq', 'assets/tree-icons/threadCreatepoint-odd.gif');
+        cy.wrap($node).find('img:nth-child(5)').invoke('attr', 'src').should('eq', 'assets/tree-icons/threadStartpoint-error-odd.gif');
+        // TODO: Shouldn't this be:
+        // cy.wrap($node).find('img:nth-child(5)').invoke('attr', 'src').should('eq', 'assets/tree-icons/threadCreatepoint-odd.gif');
       });
       cy.get('#CloseButton').click();
       cy.get('div.treeview > ul > li').should('have.length', 0);
       cy.get('#SettingsButton').should('be.visible').click();
       cy.get('#openInProgressNo').type('{backspace}');
-      cy.get('#openInProgressNo').should('have.text', ''),
+      cy.get('#openInProgressNo').should('have.text', '');
       // The second report, not two reports
       cy.get('#openInProgressNo').type('2');
-      cy.get('#buttonOpenInProgressNo').click();
+      cy.get('#openReportInProgressButton').click();
       cy.get('#debugSettingsModalClose').click();
       cy.get('div.treeview > ul > li').should('have.length', 3);
       cy.get('#CloseButton').click();
@@ -64,7 +69,7 @@ describe('Test running reports', function() {
       cy.get('#openInProgressNo').should('have.text', ''),
       // The third report, should not exist
       cy.get('#openInProgressNo').type('3');
-      cy.get('#buttonOpenInProgressNo').click();
+      cy.get('#openReportInProgressButton').click();
       cy.get('#debugSettingsModalClose').click();
       cy.get('div.treeview > ul > li').should('have.length', 0);
     });
