27b1a5dda683d4f640eed5a5335abe6005a97ac4
celmoussaoui <C.ElMoussaoui@student.tudelft.nl>
2022-07-18 13:02:55 +0200

    Add compareData provider

--- src/app/compare/compare.component.spec.ts
+++ src/app/compare/compare.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CompareComponent } from './compare.component';
+import { CompareComponent, CompareData } from './compare.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CompareComponent', () => {
@@ -10,6 +10,7 @@ describe('CompareComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CompareComponent],
+      providers: [CompareData],
       imports: [HttpClientTestingModule],
     }).compileComponents();
   });
