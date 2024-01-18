1681d44a31deac04e39d541a7e023996e857f543
MatthijsSmets <s1122126@student.hsleiden.nl>
2024-01-18 16:09:25 +0100

    test: create test for show multiple files in file tree

--- cypress/e2e/settings/settings-component.cy.ts
+++ cypress/e2e/settings/settings-component.cy.ts
@@ -26,4 +26,37 @@ describe('Tests for settings component', () => {
     cy.get('@saveButton').click();
     cy.get('@tableCell').should('have.attr', 'style', 'padding: 0em 0px;');
   });
+
+  it('should allow multiple files to be opened in debug tree when setting is enabled and close all but one report when setting is disabled', () => {
+    cy.createReport();
+    cy.get('[data-cy-open-settings-modal]').as('openSettingsModal').click();
+    cy.get('[data-cy-toggle-show-amount]').should(
+      'have.attr',
+      'value',
+      'false'
+    );
+    cy.get('[data-cy-close-settings-modal]').click();
+    cy.get('[data-cy-record-table-index="0"]').click();
+    cy.get('[data-cy-record-table-index="1"]').click();
+    cy.get('#debug-tree .jqx-tree-dropdown-root > li').should(
+      'have.length',
+      '1'
+    );
+    cy.get('@openSettingsModal').click();
+    cy.get('[data-cy-toggle-show-amount]').click();
+    cy.get('[data-cy-toggle-show-amount]').should('have.attr', 'value', 'true');
+    cy.get('[data-cy-close-settings-modal]').click();
+    cy.get('[data-cy-record-table-index="0"]').click();
+    cy.get('#debug-tree .jqx-tree-dropdown-root > li').should(
+      'have.length',
+      '2'
+    );
+    cy.get('[data-cy-open-settings-modal]').as('openSettingsModal').click();
+    cy.get('[data-cy-toggle-show-amount]').click();
+    cy.get('[data-cy-close-settings-modal]').click();
+    cy.get('#debug-tree .jqx-tree-dropdown-root > li').should(
+      'have.length',
+      '1'
+    );
+  });
 });
