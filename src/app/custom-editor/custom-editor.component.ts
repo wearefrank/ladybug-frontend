8251aab4b0658f1630c14a65764a9a5adbffb2d7
MatthijsSmets <s1122126@student.hsleiden.nl>
2024-01-25 16:46:43 +0100

    chore: add debounceTime function to not call function on every keystroke

--- src/app/custom-editor/custom-editor.component.ts
+++ src/app/custom-editor/custom-editor.component.ts
@@ -1,7 +1,7 @@
 import { Component, HostListener, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import * as prettierPluginHtml from 'prettier/plugins/html';
 import * as prettier from 'prettier';
-import { Subject, Subscription } from 'rxjs';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 import { SettingsService } from '../shared/services/settings.service';
 
 export const editorViewsConst = ['raw', 'xml'] as const;
@@ -13,9 +13,11 @@ export type EditorView = (typeof editorViewsConst)[number];
   styleUrl: './custom-editor.component.css',
 })
 export class CustomEditorComponent implements OnInit, OnDestroy {
+  @Input() height!: number;
+  @Output() saveReport: Subject<string> = new Subject<string>();
   protected readonly editorViewsConst = editorViewsConst;
   unsavedChanges: boolean = false;
-  readOnlyMode: boolean = true; //Set to true for now until save and rerun is implemented
+  readOnlyMode: boolean = false; //Set to true for now until save and rerun is implemented
   options: any = {
     theme: 'vs-light',
     language: 'xml',
@@ -33,10 +35,19 @@ export class CustomEditorComponent implements OnInit, OnDestroy {
   isPrettified: boolean = false;
   currentView: EditorView = 'raw';
   editorFocused: boolean = false;
-  @Output() saveReport: Subject<string> = new Subject<string>();
-  @Input() height!: number;
   showPrettifyOnLoad: boolean = true;
   showPrettifyOnLoadSubscription!: Subscription;
+  editorChangesSubject: Subject<string> = new Subject<string>();
+
+  @HostListener('window:keydown', ['$event'])
+  onSave(event: KeyboardEvent): void {
+    if ((event.ctrlKey || event.metaKey) && event.key == 's') {
+      event.preventDefault();
+      if (this.unsavedChanges) {
+        this.save();
+      }
+    }
+  }
 
   constructor(private settingsService: SettingsService) {}
 
@@ -44,24 +55,31 @@ export class CustomEditorComponent implements OnInit, OnDestroy {
     this.showPrettifyOnLoadSubscription = this.settingsService.prettifyOnLoadObservable.subscribe((value: boolean) => {
       this.showPrettifyOnLoad = value;
     });
+    this.subscribeToEditorChanges();
   }
 
   ngOnDestroy(): void {
     this.showPrettifyOnLoadSubscription.unsubscribe();
   }
 
+  subscribeToEditorChanges(): void {
+    this.editorChangesSubject.pipe(debounceTime(300)).subscribe((value) => {
+      this.checkIfTextIsPretty();
+    });
+  }
+
   initEditor(editor: any): void {
     if (editor) {
-      editor.onDidFocusEditorWidget(() => {
+      editor.onDidFocusEditorWidget((): void => {
         this.editorFocused = true;
       });
-
-      editor.onDidBlurEditorWidget(() => {
+      editor.onDidBlurEditorWidget((): void => {
         this.editorFocused = false;
       });
+
       this.checkIfTextIsPretty();
       if (this.showPrettifyOnLoad) {
-        this.onViewChange(editorViewsConst[1]);
+        this.onViewChange('xml');
       }
     }
   }
@@ -108,16 +126,6 @@ export class CustomEditorComponent implements OnInit, OnDestroy {
     }
   }
 
-  @HostListener('window:keydown', ['$event'])
-  onSave(event: KeyboardEvent): void {
-    if ((event.ctrlKey || event.metaKey) && event.key == 's') {
-      event.preventDefault();
-      if (this.unsavedChanges) {
-        this.save();
-      }
-    }
-  }
-
   save(): void {
     if (this.editorContent) {
       this.saveReport.next(this.editorContent);
@@ -125,6 +133,7 @@ export class CustomEditorComponent implements OnInit, OnDestroy {
   }
 
   onChange(event: string): void {
+    this.editorChangesSubject.next(event);
     this.unsavedChanges = event != this.editorContentCopy;
   }
 
@@ -134,7 +143,7 @@ export class CustomEditorComponent implements OnInit, OnDestroy {
     this.rawFile = value;
     this.checkIfTextIsPretty();
     if (this.showPrettifyOnLoad) {
-      this.onViewChange(editorViewsConst[1]);
+      this.onViewChange('xml');
     }
   }
 
