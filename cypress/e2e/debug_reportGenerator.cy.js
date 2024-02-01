de2ad54e2c0c075c7197666cb1b2333a48e8006f
Martijn Dirkse <martijn@integrationpartners.nl>
2024-02-01 15:52:33 +0100

    More items data-cy

--- cypress/e2e/debug_reportGenerator.cy.js
+++ cypress/e2e/debug_reportGenerator.cy.js
@@ -6,7 +6,7 @@ describe('Report generator', function() {
     cy.get("[data-cy-debug='openSettings']").should('be.visible').click();
     cy.get('[role=dialog]').should('be.visible', {timeout: 10000});
     cy.get('select[formcontrolname=generatorEnabled]').select('Enabled').should('have.value', 'Enabled');
-    cy.get('button[title="Save changes"').click();
+    cy.get("[data-cy-settings='saveChanges']").click();
   });
 
   it('disable and enable', function() {
@@ -19,7 +19,7 @@ describe('Report generator', function() {
     cy.get("[data-cy-debug='openSettings']").click();
     cy.get('[role=dialog]').should('be.visible', {timeout: 10000});
     cy.get('select[formcontrolname=generatorEnabled]').select('Disabled').should('have.value', 'Disabled');
-    cy.get('button[title="Save changes"').click();
+    cy.get("[data-cy-settings='saveChanges']").click();
     cy.contains('Settings saved', {timeout: 10000});
     cy.createOtherReport();
     // If we do not wait here, we do not test properly that no report is created.
@@ -31,7 +31,7 @@ describe('Report generator', function() {
     cy.get("[data-cy-debug='openSettings']").click();
     cy.get('[role=dialog]').should('be.visible', {timeout: 10000});
     cy.get('select[formcontrolname=generatorEnabled]').select('Enabled').should('have.value', 'Enabled');
-    cy.get('button[title="Save changes"').click();
+    cy.get("[data-cy-settings='saveChanges']").click();
     cy.contains('Settings saved', {timeout: 10000});
     cy.createOtherReport();
     cy.get("[data-cy-debug='refresh']").click();
