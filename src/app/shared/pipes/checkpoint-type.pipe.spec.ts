ff09a0515d095a2155d17a45ba2ed60380d424fd
MatthijsSmets <s1122126@student.hsleiden.nl>
2024-02-14 23:34:10 +0100

    chore: remove spec file for pipe

--- src/app/shared/pipes/checkpoint-type.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CheckpointTypePipe } from './checkpoint-type.pipe';
-
-describe('CheckpointTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CheckpointTypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
