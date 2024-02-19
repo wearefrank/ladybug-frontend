90067d14565ea06d7d8edd532ce385b91aa07f8b
Martijn Dirkse <martijn@integrationpartners.nl>
2024-02-19 11:26:30 +0100

    Fix quotes

--- cypress/e2e/copying.cy.js
+++ cypress/e2e/copying.cy.js
@@ -1,37 +1,37 @@
-describe("Tests about copying", function () {
+describe('Tests about copying', function () {
   beforeEach(() => {
     cy.initializeApp();
     cy.clearDebugStore();
   });
 
   afterEach(() => {
-    cy.get("[data-cy-nav-tab='testTab']").click();
-    cy.get("[data-cy-test='selectAll']").click();
-    cy.get("[data-cy-test='deleteSelected']").click();
-    cy.get("[data-cy-delete-modal='confirm']").click();
-    cy.get("[data-cy-nav-tab='debugTab']").click();
+    cy.get('[data-cy-nav-tab="testTab"]').click();
+    cy.get('[data-cy-test="selectAll"]').click();
+    cy.get('[data-cy-test="deleteSelected"]').click();
+    cy.get('[data-cy-delete-modal="confirm"]').click();
+    cy.get('[data-cy-nav-tab="debugTab"]').click();
   });
 
-  it("Copy report to test tab", () => {
-    cy.visit("");
-    cy.get("[data-cy-nav-tab='testTab']").click();
+  it('Copy report to test tab', () => {
+    cy.visit('');
+    cy.get('[data-cy-nav-tab="testTab"]').click();
     cy.checkTestTableNumRows(0);
     cy.createReport();
-    cy.get("[data-cy-nav-tab='debugTab']").click();
+    cy.get('[data-cy-nav-tab="debugTab"]').click();
     cy.checkTableNumRows(0);
-    cy.get("[data-cy-debug='refresh']").click();
+    cy.get('[data-cy-debug="refresh"]').click();
     cy.checkTableNumRows(1);
-    cy.get("[data-cy-debug='selectAll']").click();
-    cy.get("[data-cy-debug='openSelected']").click();
-    cy.get("[data-cy-debug-tree='root'] .jqx-tree-dropdown-root > li").should("have.length", 1);
-    cy.get("[data-cy-debug-editor='copy']").click();
-    cy.get("[data-cy-nav-tab='testTab']").click();
+    cy.get('[data-cy-debug="selectAll"]').click();
+    cy.get('[data-cy-debug="openSelected"]').click();
+    cy.get('[data-cy-debug-tree="root"] .jqx-tree-dropdown-root > li').should('have.length', 1);
+    cy.get('[data-cy-debug-editor="copy"]').click();
+    cy.get('[data-cy-nav-tab="testTab"]').click();
     // We test that the user does not have to refresh here.
     cy.checkTestTableReportsAre(["Simple report"]);
-    cy.get("[data-cy-nav-tab='debugTab']").click();
+    cy.get('[data-cy-nav-tab="debugTab"]').click();
     cy.checkTableNumRows(1);
-    cy.get("[data-cy-debug-tree='root'] .jqx-tree-dropdown-root > li").should("have.length", 1);
-    cy.get("[data-cy-nav-tab='testTab']").click();
+    cy.get('[data-cy-debug-tree="root"] .jqx-tree-dropdown-root > li').should('have.length', 1);
+    cy.get('[data-cy-nav-tab="testTab"]').click();
     // Do not refresh. The test tab should have saved its state.
     cy.checkTestTableReportsAre(["Simple report"]);
   });
--- cypress/e2e/debug_labels.cy.js
+++ cypress/e2e/debug_labels.cy.js
@@ -13,28 +13,28 @@ describe('Test labels', () => {
     cy.get('[data-cy-debug=\'selectAll\']').click();
     cy.get('[data-cy-debug=\'openSelected\']').click();
     cy.wait(300);
-    cy.get("[data-cy-debug-tree='root'] .jqx-tree-dropdown-root > li").should("have.length", 1);
-    testTreeView("Message is null", "Null String");
+    cy.get('[data-cy-debug-tree="root"] .jqx-tree-dropdown-root > li').should('have.length', 1);
+    testTreeView('Message is null', 'Null String');
   });
 
   it('Test label empty string', () => {
     cy.createReportWithLabelEmpty();
     cy.initializeApp();
-    cy.get("[data-cy-debug='selectAll']").click();
-    cy.get("[data-cy-debug='openSelected']").click();
-    cy.get("[data-cy-debug-tree='root'] .jqx-tree-dropdown-root > li").should("have.length", 1);
-    testTreeView("Message is an empty string", "Empty String");
+    cy.get('[data-cy-debug="selectAll"]').click();
+    cy.get('[data-cy-debug="openSelected"]').click();
+    cy.get('[data-cy-debug-tree="root"] .jqx-tree-dropdown-root > li').should('have.length', 1);
+    testTreeView('Message is an empty string', 'Empty String');
   });
 });
 
 function testTreeView(reportName, labelString) {
-  cy.get("[data-cy-debug-tree='root'] .jqx-tree-dropdown-root > li > div").within(
+  cy.get('[data-cy-debug-tree="root"] .jqx-tree-dropdown-root > li > div').within(
     ($node) => {
     cy.wrap($node).contains(reportName);
   });
-  cy.get("[data-cy-debug-tree='root'] .jqx-tree-dropdown-root > li > ul > li > div").within(
+  cy.get('[data-cy-debug-tree="root"] .jqx-tree-dropdown-root > li > ul > li > div').within(
     function ($node) {
-      cy.wrap($node).should("contain", reportName);
+      cy.wrap($node).should('contain', reportName);
       cy.wrap($node)
         .find('img')
         .invoke('attr', 'src')
@@ -42,18 +42,18 @@ function testTreeView(reportName, labelString) {
     },
   );
   cy.get(
-    "[data-cy-debug-tree='root'] .jqx-tree-dropdown-root > li > ul > li > ul > li:nth-child(1) > div"
+    '[data-cy-debug-tree="root"] .jqx-tree-dropdown-root > li > ul > li > ul > li:nth-child(1) > div'
   ).within(function ($node) {
-    cy.wrap($node).should("have.text", labelString);
+    cy.wrap($node).should('have.text', labelString);
     cy.wrap($node)
       .find('img')
       .invoke('attr', 'src')
       .should('eq', 'assets/tree-icons/infopoint-odd.gif');
   });
   cy.get(
-    "[data-cy-debug-tree='root'] .jqx-tree-dropdown-root > li > ul > li > ul > li:nth-child(2) > div"
+    '[data-cy-debug-tree="root"] .jqx-tree-dropdown-root > li > ul > li > ul > li:nth-child(2) > div'
   ).within(function ($node) {
-    cy.wrap($node).should("have.text", reportName);
+    cy.wrap($node).should('have.text', reportName);
     cy.wrap($node)
       .find('img')
       .invoke('attr', 'src')
