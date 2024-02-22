6ed33c569ad42d317a0484a1c9d988a2e1b7ac7e
MatthijsSmets <s1122126@student.hsleiden.nl>
2024-02-22 14:28:33 +0100

    feat: metadata table also shows keys where value is empty or null

--- src/app/shared/components/display-table/display-table.component.ts
+++ src/app/shared/components/display-table/display-table.component.ts
@@ -1,10 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-display-table',
   templateUrl: './display-table.component.html',
   styleUrls: ['./display-table.component.css'],
 })
-export class DisplayTableComponent {
+export class DisplayTableComponent implements OnChanges {
   @Input() report!: any;
+  anyMessagesPresent: boolean = false;
+
+  ngOnChanges(): void {
+    this.checkIfAnyMessagesPresent();
+  }
+
+  checkIfAnyMessagesPresent(): void {
+    this.anyMessagesPresent = !!(
+      !this.report.xml &&
+      (this.report.noCloseReceivedForStream ||
+        this.report.streaming ||
+        this.report.stubbed ||
+        !this.report.message ||
+        this.report.encoding ||
+        (this.report.preTruncatedMessage && this.report.preTruncatedMessageLength.length > 0) ||
+        this.report.stubNotFound)
+    );
+  }
 }
