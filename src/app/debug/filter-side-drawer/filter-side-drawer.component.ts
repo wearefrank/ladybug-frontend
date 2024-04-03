58c5b5fce82903626f40796c5de5bfe97d92b14a
MarkvdVorst <marcvorst@gmail.com>
2024-04-03 16:12:53 +0200

    added animation for the filter-side-drawer

--- src/app/debug/filter-side-drawer/filter-side-drawer.component.ts
+++ src/app/debug/filter-side-drawer/filter-side-drawer.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnDestroy } from '@angular/core';
 import { FilterService } from './filter.service';
-import { Subscriber, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-filter-side-drawer',
   templateUrl: './filter-side-drawer.component.html',
   styleUrl: './filter-side-drawer.component.css',
+  animations: [
+    trigger('removeTrigger', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('300ms ease-in', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', animate('300ms ease-out', style({ transform: 'translateX(100%)' }))),
+    ]),
+  ],
 })
 export class FilterSideDrawerComponent implements OnDestroy {
-  protected shouldShowFilter: boolean = false;
+  protected shouldShowFilter!: boolean;
   shouldShowFilterSubscriber: Subscription;
 
   constructor(private filterService: FilterService) {
