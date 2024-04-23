adafa1ad62d3bf9a5a2fe83165bf13a1860b0ab1
MatthijsSmets <s1122126@student.hsleiden.nl>
2024-04-23 16:11:48 +0200

    chore: fix unit test import

--- src/app/shared/components/file-tree/file-tree.component.spec.ts
+++ src/app/shared/components/file-tree/file-tree.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FileTreeComponent } from './file-tree.component';
+import { NgSimpleFileTreeModule } from 'ng-simple-file-tree';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('FileTreeComponent', () => {
   let component: FileTreeComponent;
@@ -9,6 +11,7 @@ describe('FileTreeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FileTreeComponent],
+      imports: [HttpClientTestingModule, NgSimpleFileTreeModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FileTreeComponent);
