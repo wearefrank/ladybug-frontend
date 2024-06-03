1b3ecf66832646eecfe55c9b83ef7cbb91b478f3
MarkvdVorst <marcvorst@gmail.com>
2024-06-03 15:18:04 +0200

    chore: solved a small bug where it would show two forward slashes instead of one in name field of test report

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -343,7 +343,7 @@ export class TestComponent implements OnInit, AfterViewInit {
 
   getFullPath(path: string, name: string): string {
     if (path) {
-      return `${path.replace(this.currentFilter, '')}/${name}`;
+      return `${path.replace(this.currentFilter, '')}${name}`;
     }
     return `/${name}`;
   }
