90a44c6804a6f9deb7a538a6737bb92ed7d13697
MarkvdVorst <34445874+MarkvdVorst@users.noreply.github.com>
2024-06-03 10:02:17 +0200

    Error shown for wrong user input in filter (#403)
    * chore: set up the first functionalities for the filter type error
    
    * chore: small fix for push and typo fix
    
    * chore: created the rest of the functionalities and html elements for this issue
    also cleaned up some code
    
    * chore: removed the filterErrorsSubscriber and filters from filter-side-drawer component as it was not used anywhere
    
    * chore: made the error shown more user-friendly
    changed test accordingly
    
    * chore: added template literal for showing string with variables
    
    * chore: added default to typeLabel in table component and encapsulated the validator if-statements in filter service
    
    * chore: added undefined back to subscription in table.component.ts and filter-side-drawer.component.ts
    
    * chore: added suggested changes from review
    
    * chore: added suggested change to separate the adding of ', '

--- src/app/debug/filter-side-drawer/filter.service.ts
+++ src/app/debug/filter-side-drawer/filter.service.ts
@@ -1,27 +1,55 @@
 import { Injectable } from '@angular/core';
-import { debounceTime, Observable, Subject } from 'rxjs';
+import { debounceTime, filter, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FilterService {
   private showFilterSubject: Subject<boolean> = new Subject();
-  private metadataNamesSubject: Subject<string[]> = new Subject();
+  private metadataLabelsSubject: Subject<string[]> = new Subject();
   private filterContextSubject: Subject<Map<string, string>> = new Subject();
   private currentRecordsSubject: Subject<Map<string, Array<string>>> = new Subject();
+  private metadataTypesSubject: Subject<Map<string, string>> = new Subject();
+  private filterErrorSubject: Subject<[boolean, Map<string, string>]> = new Subject();
   private filters: Map<string, string> = new Map<string, string>();
+  private metadataTypes: Map<string, string> = new Map<string, string>();
+  private filterErrors: Map<string, string> = new Map<string, string>();
 
   showFilter$: Observable<boolean> = this.showFilterSubject.asObservable();
-  metadataNames$: Observable<string[]> = this.metadataNamesSubject.asObservable();
-  filterContext$: Observable<Map<string, string>> = this.filterContextSubject.pipe(debounceTime(300));
+  metadataLabels$: Observable<string[]> = this.metadataLabelsSubject.asObservable();
   currentRecords$: Observable<Map<string, Array<string>>> = this.currentRecordsSubject.asObservable();
+  metadataTypes$: Observable<Map<string, string>> = this.metadataTypesSubject.asObservable();
+  filterError$: Observable<[boolean, Map<string, string>]> = this.filterErrorSubject.asObservable();
+  filterContext$: Observable<Map<string, string>> = this.filterContextSubject.pipe(
+    debounceTime(300),
+    filter((context: Map<string, string>): boolean => {
+      if (context.size === 0) {
+        this.disableFilterError();
+        return true;
+      }
+      let errorFound: boolean = false;
+      this.filterErrors.clear();
+      for (let [key, value] of context) {
+        if (this.isInValidTimestamp(value, key) || this.isInValidNumber(value, key)) {
+          this.filterErrors.set(<string>this.metadataTypes.get(key), value);
+          errorFound = true;
+        }
+      }
+      if (errorFound) {
+        this.enableFilterError(this.filterErrors);
+      } else {
+        this.disableFilterError();
+      }
+      return !errorFound;
+    }),
+  );
 
   setShowFilter(show: boolean): void {
     this.showFilterSubject.next(show);
   }
 
-  setMetadataNames(metadataNames: string[]): void {
-    this.metadataNamesSubject.next(metadataNames);
+  setMetadataLabels(metadataLabels: string[]): void {
+    this.metadataLabelsSubject.next(metadataLabels);
   }
 
   updateFilterContext(filterName: string, filterContext: string): void {
@@ -41,5 +69,33 @@ export class FilterService {
 
   setCurrentRecords(records: Map<string, Array<string>>): void {
     this.currentRecordsSubject.next(records);
+  }
+
+  setMetadataTypes(metadataTypes: Map<string, string>): void {
+    this.metadataTypes = new Map<string, string>(Object.entries(metadataTypes));
+    this.metadataTypesSubject.next(this.metadataTypes);
+  }
+
+  disableFilterError(): void {
+    this.filterErrors.clear();
+    this.filterErrorSubject.next([false, this.filterErrors]);
+  }
+
+  enableFilterError(inputFilter: Map<string, string>): void {
+    this.filterErrors = inputFilter;
+    this.filterErrorSubject.next([true, this.filterErrors]);
+  }
+
+  isInValidTimestamp(userInput: string, metadataName: string): boolean {
+    return this.metadataTypes.get(metadataName) == 'timestamp' && Number.isNaN(Date.parse(userInput));
+  }
+
+  isInValidNumber(userInput: string, metadataName: string): boolean {
+    const regex: RegExp = /^-?\d+(\.\d+)?$/;
+    const metadataType = this.metadataTypes.get(metadataName);
+    return (
+      (metadataType == 'int' || metadataType == 'long') &&
+      (Number.isNaN(Number.parseFloat(userInput)) || !regex.test(userInput))
+    );
   }
 }
