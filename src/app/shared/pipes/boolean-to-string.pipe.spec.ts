2fc4ba1cbae063e88ffd775e52a8eb1bbb72f154
Martijn Dirkse <martijn@integrationpartners.nl>
2024-06-17 17:46:27 +0200

    Upgrade dependencies

--- src/app/shared/pipes/boolean-to-string.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BooleanToStringPipe } from './boolean-to-string.pipe';
-
-describe('BooleanToStringPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BooleanToStringPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/shared/pipes/table-cell-shortener.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TableCellShortenerPipe } from './table-cell-shortener.pipe';
-
-describe('TableCellShortenerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TableCellShortenerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
