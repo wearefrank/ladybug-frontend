8a2f4642e93272ce3d7c7cc90f5d09262f434996
MatthijsSmets <s1122126@student.hsleiden.nl>
2024-07-24 16:20:11 +0200

    refactor: added types to meta-data table and removed unused code

--- src/app/shared/components/display-table/metadata-table.component.ts
+++ src/app/shared/components/display-table/metadata-table.component.ts
@@ -1,34 +1,40 @@
 import { Component, Input, OnChanges } from '@angular/core';
-import { CheckpointTypePipe } from '../../pipes/checkpoint-type.pipe';
 import { ClipboardModule } from '@angular/cdk/clipboard';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { CopyTooltipDirective } from '../../directives/copy-tooltip.directive';
+import { Report } from '../../interfaces/report';
+import { Checkpoint } from '../../interfaces/checkpoint';
+import { ReportUtil } from '../../util/report-util';
 
 @Component({
   selector: 'app-display-table',
   templateUrl: './metadata-table.component.html',
   styleUrls: ['./metadata-table.component.css'],
   standalone: true,
-  imports: [ClipboardModule, CheckpointTypePipe, MatTooltipModule, CopyTooltipDirective],
+  imports: [ClipboardModule, MatTooltipModule, CopyTooltipDirective],
 })
 export class MetadataTableComponent implements OnChanges {
-  @Input() report!: any;
+  protected readonly ReportUtil = ReportUtil;
+
+  @Input() report!: Report | Checkpoint;
+
   anyMessagesPresent: boolean = false;
 
   ngOnChanges(): void {
     this.checkIfAnyMessagesPresent();
+    console.log(this.report);
   }
 
   checkIfAnyMessagesPresent(): void {
-    this.anyMessagesPresent = !!(
-      !this.report.xml &&
-      (this.report.noCloseReceivedForStream ||
+    if (ReportUtil.isCheckPoint(this.report)) {
+      this.anyMessagesPresent = !!(
         this.report.streaming ||
         this.report.stubbed ||
         !this.report.message ||
         this.report.encoding ||
-        (this.report.preTruncatedMessage && this.report.preTruncatedMessageLength.length > 0) ||
-        this.report.stubNotFound)
-    );
+        (this.report.preTruncatedMessageLength && this.report.preTruncatedMessageLength > 0) ||
+        this.report.stubNotFound
+      );
+    }
   }
 }
