34cdf1302aa9b80c6ad697558f227207adcdd5e1
MarkvdVorst <marcvorst@gmail.com>
2024-07-29 13:26:41 +0200

    chore: Added some conventional changes and feedback changes Added types where possible. To add types for the api response, I created an interface with the field "status". Changed a foreach loop to the more conventional for of loop. Removed the url paramater for interceptGetApiCall as the same one is used every time.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -10,7 +10,7 @@
 // ***********************************************
 
 import Chainable = Cypress.Chainable;
-import { StringMatcher } from 'cypress/types/net-stubbing';
+import { Interception } from 'cypress/types/net-stubbing';
 import JQueryWithSelector = Cypress.JQueryWithSelector;
 
 declare global {
@@ -45,107 +45,107 @@ declare global {
   }
 }
 
-Cypress.Commands.add('initializeApp' as keyof Chainable, () => {
+Cypress.Commands.add('initializeApp' as keyof Chainable, (): void => {
   //Custom command to initialize app and wait for all api requests
-  interceptGetApiCall(/\/api\/*?/g, 'apiCall');
+  interceptGetApiCall('apiCall');
   cy.visit('');
   cy.wait('@apiCall').then(() => cy.log('All api requests have completed'));
 });
 
-Cypress.Commands.add('resetApp' as keyof Chainable, () => {
+Cypress.Commands.add('resetApp' as keyof Chainable, (): void => {
   cy.clearDebugStore();
   cy.clearTestReports();
   cy.clearReportsInProgress();
   cy.initializeApp();
 });
 
-Cypress.Commands.add('clearTestReports' as keyof Chainable, () => {
-  cy.request({ method: 'DELETE', url: '/api/report/all/Test' }).then((resp) => {
+Cypress.Commands.add('clearTestReports' as keyof Chainable, (): void => {
+  cy.request({ method: 'DELETE', url: '/api/report/all/Test' }).then((resp: Cypress.Response<ApiResponse>) => {
     expect(resp.status).equal(200);
   });
 });
 
-Cypress.Commands.add('navigateToTestTabAndWait' as keyof Chainable, () => {
+Cypress.Commands.add('navigateToTestTabAndWait' as keyof Chainable, (): void => {
   navigateToTabAndWait('test');
 });
 
-Cypress.Commands.add('navigateToDebugTabAndWait' as keyof Chainable, () => {
+Cypress.Commands.add('navigateToDebugTabAndWait' as keyof Chainable, (): void => {
   navigateToTabAndWait('debug');
 });
 
-Cypress.Commands.add('createReport' as keyof Chainable, () => {
+Cypress.Commands.add('createReport' as keyof Chainable, (): void => {
   // No cy.visit because then the API call can happen multiple times.
-  cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Simple%20report`).then((resp) => {
+  cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Simple%20report`).then((resp: Cypress.Response<ApiResponse>) => {
     expect(resp.status).equal(200);
   });
 });
 
-Cypress.Commands.add('createOtherReport' as keyof Chainable, () => {
+Cypress.Commands.add('createOtherReport' as keyof Chainable, (): void => {
   // No cy.visit because then the API call can happen multiple times.
-  cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Another%20simple%20report`).then((resp) => {
+  cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Another%20simple%20report`).then((resp: Cypress.Response<ApiResponse>) => {
     expect(resp.status).equal(200);
   });
 });
 
-Cypress.Commands.add('createRunningReport' as keyof Chainable, () => {
+Cypress.Commands.add('createRunningReport' as keyof Chainable, (): void => {
   cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Waiting%20for%20thread%20to%20start`).then(
-    (resp) => {
+    (resp: Cypress.Response<ApiResponse>): void => {
       expect(resp.status).equal(200);
     },
   );
 });
 
-Cypress.Commands.add('createReportWithLabelNull' as keyof Chainable, () => {
+Cypress.Commands.add('createReportWithLabelNull' as keyof Chainable, (): void => {
   // No cy.visit because then the API call can happen multiple times.
-  cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Message%20is%20null`).then((resp) => {
+  cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Message%20is%20null`).then((resp: Cypress.Response<ApiResponse>) => {
     expect(resp.status).equal(200);
   });
 });
 
-Cypress.Commands.add('createReportWithLabelEmpty' as keyof Chainable, () => {
+Cypress.Commands.add('createReportWithLabelEmpty' as keyof Chainable, (): void => {
   // No cy.visit because then the API call can happen multiple times.
   cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Message%20is%20an%20empty%20string`).then(
-    (resp) => {
+    (resp: Cypress.Response<ApiResponse>): void => {
       expect(resp.status).equal(200);
     },
   );
 });
 
-Cypress.Commands.add('createReportWithInfopoint' as keyof Chainable, () => {
+Cypress.Commands.add('createReportWithInfopoint' as keyof Chainable, (): void => {
   // No cy.visit because then the API call can happen multiple times.
   cy.request(
     `${Cypress.env('backendServer')}/index.jsp?createReport=Hide%20a%20checkpoint%20in%20blackbox%20view`,
-  ).then((resp) => {
+  ).then((resp: Cypress.Response<ApiResponse>): void => {
     expect(resp.status).equal(200);
   });
 });
 
-Cypress.Commands.add('createReportWithMultipleStartpoints' as keyof Chainable, () => {
+Cypress.Commands.add('createReportWithMultipleStartpoints' as keyof Chainable, (): void => {
   // No cy.visit because then the API call can happen multiple times.
-  cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Multiple%20startpoints`).then((resp) => {
+  cy.request(`${Cypress.env('backendServer')}/index.jsp?createReport=Multiple%20startpoints`).then((resp: Cypress.Response<ApiResponse>): void => {
     expect(resp.status).equal(200);
   });
 });
 
-Cypress.Commands.add('clearDebugStore' as keyof Chainable, () => {
-  cy.request(`${Cypress.env('backendServer')}/index.jsp?clearDebugStorage=true`).then((resp) => {
+Cypress.Commands.add('clearDebugStore' as keyof Chainable, (): void => {
+  cy.request(`${Cypress.env('backendServer')}/index.jsp?clearDebugStorage=true`).then((resp: Cypress.Response<ApiResponse>): void => {
     expect(resp.status).equal(200);
   });
 });
 
-Cypress.Commands.add('clearReportsInProgress' as keyof Chainable, () => {
-  cy.request(`${Cypress.env('backendServer')}/index.jsp?removeReportsInProgress`).then((resp) => {
+Cypress.Commands.add('clearReportsInProgress' as keyof Chainable, (): void => {
+  cy.request(`${Cypress.env('backendServer')}/index.jsp?removeReportsInProgress`).then((resp: Cypress.Response<ApiResponse>): void => {
     expect(resp.status).equal(200);
   });
 });
 
-Cypress.Commands.add('selectIfNotSelected' as keyof Chainable, { prevSubject: 'element' }, (node: JQueryWithSelector<HTMLElement>) => {
+Cypress.Commands.add('selectIfNotSelected' as keyof Chainable, { prevSubject: 'element' }, (node: JQueryWithSelector<HTMLElement>): void => {
   if (!node[0].classList.contains("selected")) {
     cy.wrap(node).click()
   }
 });
 
-Cypress.Commands.add('enableShowMultipleInDebugTree' as keyof Chainable, () => {
+Cypress.Commands.add('enableShowMultipleInDebugTree' as keyof Chainable, (): void => {
   cy.get('[data-cy-debug="openSettings"]').click();
   cy.get('[data-cy-settings="showAmount"]').click();
   cy.get('[data-cy-settings="saveChanges"]').click();
@@ -157,12 +157,12 @@ Cypress.Commands.add('checkTestTableNumRows' as keyof Chainable, (length: number
 
 Cypress.Commands.add('checkTestTableReportsAre' as keyof Chainable, (reportNames: string[]): void => {
   cy.checkTestTableNumRows(reportNames.length);
-  reportNames.forEach((reportName) => {
+  for (const reportName of reportNames) {
     cy.get('[data-cy-test="table"]').find('tr').contains(`/${reportName}`).should('have.length', 1);
-  });
+  }
 });
 
-Cypress.Commands.add('debugTreeGuardedCopyReport' as keyof Chainable, (reportName: string, numExpandedNodes: number, aliasSuffix: string) => {
+Cypress.Commands.add('debugTreeGuardedCopyReport' as keyof Chainable, (reportName: string, numExpandedNodes: number, aliasSuffix: string): void => {
   const alias = `debugTreeGuardedCopyReport_${aliasSuffix}`;
   cy.get('[data-cy-debug-tree="root"]')
     .find(`app-tree-item .item-name:contains(${reportName})`)
@@ -174,7 +174,7 @@ Cypress.Commands.add('debugTreeGuardedCopyReport' as keyof Chainable, (reportNam
     times: 1,
   }).as(alias);
   cy.get('[data-cy-debug-editor="copy"]').click();
-  cy.wait(`@${alias}`).then((res) => {
+  cy.wait(`@${alias}`).then((res: Interception): void => {
     cy.wrap(res).its('request.url').should('contain', 'Test');
     cy.wrap(res).its('request.body').as('requestBody');
     cy.get('@requestBody').its('Debug').should('have.length', 1);
@@ -183,49 +183,53 @@ Cypress.Commands.add('debugTreeGuardedCopyReport' as keyof Chainable, (reportNam
   });
 });
 
-Cypress.Commands.add('clickFirstFileInFileTree' as keyof Chainable, () => {
+Cypress.Commands.add('clickFirstFileInFileTree' as keyof Chainable, (): void => {
   cy.get('[data-cy-debug-tree="root"] > app-tree-item').eq(0).find('app-tree-item').eq(0).click();
 });
 
-Cypress.Commands.add('clickRowInTable' as keyof Chainable, (index: number) => {
+Cypress.Commands.add('clickRowInTable' as keyof Chainable, (index: number): void => {
   cy.get('[data-cy-debug="tableBody"]').get('tbody').find('tr').eq(index).click();
 });
 
-Cypress.Commands.add('checkFileTreeLength' as keyof Chainable, (length: number) => {
+Cypress.Commands.add('checkFileTreeLength' as keyof Chainable, (length: number): void => {
   cy.get('[data-cy-debug-tree="root"] > app-tree-item').should('have.length', length);
 });
 
-Cypress.Commands.add('refreshApp' as keyof Chainable, () => {
-  interceptGetApiCall(/\/api\/*?/g, 'apiCall');
+Cypress.Commands.add('refreshApp' as keyof Chainable, (): void => {
+  interceptGetApiCall('apiCall');
   cy.get('[data-cy-debug="refresh"]').click();
   cy.wait('@apiCall').then(() => cy.log('All api requests have completed'));
 });
 
-Cypress.Commands.add('getTableBody' as keyof Chainable, () => {
+Cypress.Commands.add('getTableBody' as keyof Chainable, (): Chainable => {
   return cy.get('[data-cy-debug="tableBody"]').get('tbody');
 });
 
-Cypress.Commands.add('assertDebugTableLength' as keyof Chainable, (length: number) => {
+Cypress.Commands.add('assertDebugTableLength' as keyof Chainable, (length: number): void => {
   length === 0
     ? cy.getTableBody().find('tr').should('not.exist')
     : cy.getTableBody().find('tr').should('have.length', length);
 });
 
-function interceptGetApiCall(url: StringMatcher, alias: string): void {
+function interceptGetApiCall(alias: string): void {
   cy.intercept({
     method: 'GET',
     hostname: 'localhost',
-    url: url,
+    url: /\/api\/*?/g,
   }).as(alias);
 }
 
 //More string values can be added for each tab that can be opened
 function navigateToTabAndWait(tab: 'debug' | 'test'): void {
   const apiCallAlias: string = `apiCall${tab}Tab`;
-  interceptGetApiCall(/\/api\/*?/g, apiCallAlias);
+  interceptGetApiCall(apiCallAlias);
   cy.visit('');
   cy.get(`[data-cy-nav-tab="${tab}Tab"]`).click();
   cy.wait(`@${apiCallAlias}`).then(() => {
     cy.log('All api requests have completed, ');
   });
+}
+
+interface ApiResponse {
+  status: boolean;
 }
