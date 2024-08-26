40cff78f1f2ee6dbe23d50b4266145bd0cf33f8d
MarkvdVorst <marcvorst@gmail.com>
2024-08-26 15:37:39 +0200

    Chore: fix for cypress test

--- cypress/e2e/debug/deleteReports.cy.ts
+++ cypress/e2e/debug/deleteReports.cy.ts
@@ -15,7 +15,7 @@ describe('About deleting reports', () => {
     cy.assertDebugTableLength(2);
     cy.get('[data-cy-debug="selectOne"]').eq(0).click();
     cy.get('[data-cy-debug="deleteSelected"]').click();
-    cy.getTableRows().first().contains('Another simple report');
+    cy.getDebugTableRows().first().contains('Another simple report');
   })
 
   it ('Should delete all reports with the deleteAll button', () => {
