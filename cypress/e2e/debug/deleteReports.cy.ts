fafe0376d38209e82cc53b3eb8532f42059b4ed6
MarkvdVorst <marcvorst@gmail.com>
2024-08-26 15:38:35 +0200

    Chore: added semi-colons

--- cypress/e2e/debug/deleteReports.cy.ts
+++ cypress/e2e/debug/deleteReports.cy.ts
@@ -16,19 +16,19 @@ describe('About deleting reports', () => {
     cy.get('[data-cy-debug="selectOne"]').eq(0).click();
     cy.get('[data-cy-debug="deleteSelected"]').click();
     cy.getDebugTableRows().first().contains('Another simple report');
-  })
+  });
 
   it ('Should delete all reports with the deleteAll button', () => {
     cy.assertDebugTableLength(2);
     cy.get('[data-cy-debug="deleteAll"]').click();
     cy.get('[data-cy-delete-modal="confirm"]').click();
     cy.assertDebugTableLength(0);
-  })
+  });
 
   it ('Should not open the delete modal when there are no reports', () => {
     cy.get('[data-cy-debug="deleteAll"]').click();
     cy.get('[data-cy-delete-modal="confirm"]').should('exist').click();
     cy.get('[data-cy-debug="deleteAll"]').click();
     cy.get('[data-cy-delete-modal="confirm"]').should('not.exist');
-  })
-})
+  });
+});
