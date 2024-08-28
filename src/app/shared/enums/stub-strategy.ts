50969d6a0f0e5d581c3191636318934ab5f780d6
Matthijs Smets <matthijs.smets@hotmail.com>
2024-08-28 14:45:07 +0200

    refactor: change let to const

--- src/app/shared/enums/stub-strategy.ts
+++ src/app/shared/enums/stub-strategy.ts
@@ -1,8 +1,8 @@
-export let StubStrategyUtil = {
+export const StubStrategyUtil = {
   reportStubStrategies: [] as string[],
   checkpointStubStrategyConst: [
     'Use report level stub strategy',
     'Always stub this checkpoint',
     'Never stub this checkpoint',
-  ],
+  ] as const,
 };
