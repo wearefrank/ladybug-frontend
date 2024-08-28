d7dd72e72dc59a668efab94f2e79960027cdb5c0
MarkvdVorst <34445874+MarkvdVorst@users.noreply.github.com>
2024-08-28 17:08:06 +0200

    Refactor of test table (#640)
    * chore: moved tbody of test table to new component
    
    * chore: added httpclient to spec
    
    * chore: Moved table in test component to its own component.
    There is still an underlying issue where backend refuses to do anything anymore. I'm still not sure what causes this, but it's no longer happening.
    
    * chore: Made it so the test table gets updated now on button presses
    
    * chore: ShowStorageId variable now gets updated
    
    * chore: Fixed an issue where matches will no longer be called a lot of times every time the path is updated. Only one call per update.
    Additionally, path field in report will now be set to '/' if it is null.
    
    * chore: Removed unused field
    
    * chore: Removed unused css field
    
    * chore: Added a few changes. See description.
    Changed names for testtab tests so it follows conventions.
    Changed the table so it no longer calls methods when trying to display data every time the page updates.
    Matches was duplicated in test-table.component.ts and is now removed.
    A lot of fields were moved to the test-list-item.ts type for better accessibility.
    Removed unnecessary method where the reference could access EventEmitters directly.
    
    * chore: Added missing functionality that was missed during merge with master later.
    Added missing disabled attribute to replaceReport as this feature is not implemented yet.
    
    * chore: make some method calls sequential
    
    * fix: test tab keeps rerun results when switching tabs
    
    * fix: linting issues
    
    * chore: Made some small changes
    reordered some class variables,
    fixed a spelling mistake,
    removed unnecessary empty string assigning,
    Moved a method from test component to the test table component
    
    * chore: Changed reports class variable to required true
    
    * feat: add cypress test to garantue functionality
    
    * chore: Moved amountselected to test table component
    
    * chore: Fix for new table functionality
    
    * chore: converted test table to mat-table without bootstrap
    
    * Chore: lint fixes
    
    * Chore: fix for cypress test
    
    * Chore: added semi-colons
    
    * chore: Changed some styling based on PR feedback
    
    * chore: Made table take up entire width
    
    * chore: vertical align header fix
    
    * fix: header width fix
    
    * chore: removed bootstrap dependency
    
    ---------
    
    Co-authored-by: Matthijs Smets <matthijs.smets@hotmail.com>

--- cypress/e2e/debug/deleteReports.cy.ts
+++ cypress/e2e/debug/deleteReports.cy.ts
@@ -15,20 +15,20 @@ describe('About deleting reports', () => {
     cy.assertDebugTableLength(2);
     cy.get('[data-cy-debug="selectOne"]').eq(0).click();
     cy.get('[data-cy-debug="deleteSelected"]').click();
-    cy.getTableRows().first().contains('Another simple report');
-  })
+    cy.getDebugTableRows().first().contains('Another simple report');
+  });
 
   it ('Should delete all reports with the deleteAll button', () => {
     cy.assertDebugTableLength(2);
     cy.get('[data-cy-debug="deleteAll"]').click();
     cy.get('[data-cy-delete-modal="confirm"]').click();
     cy.assertDebugTableLength(0);
-  })
+  });
 
   it ('Should not open the delete modal when there are no reports', () => {
     cy.get('[data-cy-debug="deleteAll"]').click();
     cy.get('[data-cy-delete-modal="confirm"]').should('exist').click();
     cy.get('[data-cy-debug="deleteAll"]').click();
     cy.get('[data-cy-delete-modal="confirm"]').should('not.exist');
-  })
-})
+  });
+});
