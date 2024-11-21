319cdcbbebd3623aee2be4e3d850576ed9ad295b
Martijn Dirkse <martijn@integrationpartners.nl>
2024-11-21 16:18:20 +0100

    Show more clearly which tests are selected

--- cypress/e2e/checks.cy.ts
+++ cypress/e2e/checks.cy.ts
@@ -1,6 +1,4 @@
 describe('Checks of the test environment', () => {
-  it('Show environment', () => {
-    cy.log('Environment variable \'selection\' is ' + Cypress.env('selection'))
-    cy.screenshot()
+  it('Environment \'selection\' is: ' + Cypress.env('selection'), () => {
   })
 })
