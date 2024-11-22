35aca5e2c7cf113c2065f9fa112c18b2c9cc5540
Martijn Dirkse <martijn@integrationpartners.nl>
2024-11-22 14:57:37 +0100

    Correct tests about XML storage

--- cypress/e2e/1 about-xml-storage.cy.ts
+++ cypress/e2e/1 about-xml-storage.cy.ts
@@ -1,3 +1,10 @@
+// This test should be run as the first. Other tests
+// have to delete all reports from the test tab and
+// would remove pre-existing reports.
+//
+// Furthermore, we need an empty test tab in case
+// there are no predefined reports.
+//
 describe('Compare with and without profile xml.storage', () => {
   beforeEach(() => {
     cy.initializeApp();
@@ -10,7 +17,9 @@ describe('Compare with and without profile xml.storage', () => {
     })
   } else {
     it('Report present in src/test/testtool should not be shown', () => {
-      cy.getTestTableRows().contains('Pre existing report').should('not.exist')
+      // We cannot search for 'Pre existing report' within the
+      // test rows when there are not table rows at all.
+      cy.getTestTableRows().should('not.exist')
     })
   }
 })
