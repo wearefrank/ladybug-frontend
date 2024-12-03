a1526cd70d43b003cb57f99042de5f573a6b4d24
Martijn Dirkse <martijn@integrationpartners.nl>
2024-12-03 11:13:46 +0100

    Use include feature of matrix to simplify

--- .github/workflows/extractColumn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Usage:
-#     extractColumn.sh <file to extract> <key> <column number to extract, starting from index 1>
-#
-grep -v -E "^#" $1 | grep "^$2" | sed 's|\s\s*| |g' | cut -d" " -f$3
