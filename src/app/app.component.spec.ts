412c7111b7eea457f658359f82f4e69e6a92b2d3
Martijn Dirkse <martijn@integrationpartners.nl>
2025-03-13 12:15:32 +0100

    Diable more linting rules

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,6 @@
+/* eslint sonarjs/no-gratuitous-expressions: 0 */
+/* eslint sonarjs/no-extra-arguments: 0 */
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
