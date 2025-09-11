3334a5456a8f2ef7ff58af4187ff7076705b894c
Martijn Dirkse <mhdirkse@pm.me>
2025-09-11 14:43:42 +0200

    Work in progress to merge components

--- src/app/report/edit-display/edit-display.component.ts
+++ src/app/report/edit-display/edit-display.component.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 import {
+  AfterViewInit,
   ChangeDetectorRef,
   Component,
   ElementRef,
@@ -50,6 +51,12 @@ import { DebugTabService } from '../../debug/debug-tab.service';
 import { StubStrategy } from '../../shared/enums/stub-strategy';
 import { ReportAlertMessageComponent } from '../report-alert-message/report-alert-message.component';
 import { Router } from '@angular/router';
+import { MonacoEditorComponent } from 'src/app/monaco-editor/monaco-editor.component';
+
+const basicContentTypes = ['raw'] as const;
+const prettyContentTypes = ['xml', 'json'] as const;
+const editorViewsConst = [...basicContentTypes, ...prettyContentTypes] as const;
+type EditorView = (typeof editorViewsConst)[number];
 
 @Component({
   selector: 'app-edit-display',
@@ -63,7 +70,6 @@ import { Router } from '@angular/router';
     NgbDropdownButtonItem,
     NgbDropdownItem,
     ReactiveFormsModule,
-    EditorComponent,
     MetadataTableComponent,
     MessagecontextTableComponent,
     BooleanToStringPipe,
@@ -75,9 +81,10 @@ import { Router } from '@angular/router';
     EncodingButtonComponent,
     FormsModule,
     ReportAlertMessageComponent,
+    MonacoEditorComponent,
   ],
 })
-export class EditDisplayComponent implements OnChanges {
+export class EditDisplayComponent implements AfterViewInit, OnChanges {
   @Input() containerHeight!: number;
   @Input({ required: true }) currentView!: View;
   @Input() newTab = true;
@@ -86,6 +93,7 @@ export class EditDisplayComponent implements OnChanges {
   @ViewChild(DifferenceModalComponent)
   differenceModal!: DifferenceModalComponent;
   @ViewChild('topComponent') topComponent?: ElementRef;
+  @ViewChild('statusBarElement') statusBar?: ElementRef;
   @ViewChild('editToggleButton') editToggleButton!: ToggleButtonComponent;
 
   editingEnabled = false;
@@ -99,12 +107,25 @@ export class EditDisplayComponent implements OnChanges {
   stub?: number;
   stubStrategy?: string;
 
+  requestedEditorContent?: string;
+
+  options: any = {
+    theme: 'vs-light',
+    language: 'xml',
+    inlineCompletionsAccessibilityVerbose: true,
+    automaticLayout: true,
+    padding: { bottom: 200 },
+    selectOnLineNumbers: true,
+    renderFinalNewline: false,
+    scrollBeyondLastLine: false,
+  };
+
   protected readonly ReportUtil = ReportUtil;
   protected readonly Number: NumberConstructor = Number;
   protected readonly StubStrategy = StubStrategy;
-  protected calculatedHeight = 340;
 
   protected appVariablesService = inject(AppVariablesService);
+  protected monacoEditorHeight!: number;
 
   private modalService = inject(NgbModal);
   private httpService = inject(HttpService);
@@ -116,11 +137,16 @@ export class EditDisplayComponent implements OnChanges {
   private cdr = inject(ChangeDetectorRef);
   private router = inject(Router);
 
+  private originalCheckpointValue?: string | null;
+  private unsavedChanges = false;
+
+  ngAfterViewInit(): void {
+    this.setMonacoEditorHeight();
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['containerHeight']) {
-      const topComponentHeight = this.topComponent ? this.topComponent?.nativeElement.offsetHeight : 47;
-      this.calculatedHeight = this.containerHeight - topComponentHeight;
-      this.cdr.detectChanges();
+      this.setMonacoEditorHeight();
     }
   }
 
@@ -429,5 +455,228 @@ export class EditDisplayComponent implements OnChanges {
           },
         });
     }
+  }
+
+  /* Copied */
+
+  isPrettified = false;
+  currentView: EditorView = 'raw';
+  editorChangesSubject: Subject<string> = new Subject<string>();
+
+  //Settings attributes
+  showPrettifyOnLoad = true;
+  availableViews!: EditorView[];
+  contentType!: EditorView;
+
+  editorFocused = false;
+
+  private actualEditorContent?: string | null;
+
+  private readonly INDENT_TWO_SPACES: string = '  ';
+  private subscriptions: Subscription = new Subscription();
+
+  private settingsService = inject(SettingsService);
+  private cdr = inject(ChangeDetectorRef);
+
+  @HostListener('window:keydown', ['$event'])
+  keyBoardListener(event: KeyboardEvent): void {
+    if ((event.ctrlKey || event.metaKey) && event.key === 's') {
+      event.preventDefault();
+      this.onSave();
+    }
+  }
+
+  ngOnInit(): void {
+    this.subscribeToEditorChanges();
+    this.subscribeToSettings();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  onFocusedChanged(focused: boolean): void {
+    this.editorFocused = focused;
+  }
+
+  onSave(): void {
+    if (this.unsavedChanges && this.requestedEditorContent) {
+      this.openDifferenceModal('save');
+    }
+  }
+
+  subscribeToSettings(): void {
+    const prettifyOnLoad: Subscription = this.settingsService.prettifyOnLoadObservable.subscribe((value: boolean) => {
+      this.showPrettifyOnLoad = value;
+    });
+    this.subscriptions.add(prettifyOnLoad);
+  }
+
+  subscribeToEditorChanges(): void {
+    /*
+    const editorChangesSubscription: Subscription = this.editorChangesSubject
+      .pipe(debounceTime(300))
+      .subscribe((value: string) => {
+        this.checkIfTextIsPretty();
+      });
+    this.subscriptions.add(editorChangesSubscription);
+    */
+    const editorChangesSubscription: Subscription = this.editorChangesSubject.subscribe((value) => {
+      if (this.requestedEditorContent !== undefined && this.requestedEditorContent !== null) {
+        this.actualEditorContent = value;
+      } else {
+        this.actualEditorContent = null;
+      }
+    });
+    this.subscriptions.add(editorChangesSubscription);
+  }
+
+  initEditor(): void {
+    this.checkIfTextIsPretty();
+    if (this.showPrettifyOnLoad) {
+      this.onViewChange(this.contentType);
+    }
+  }
+
+  checkIfTextIsPretty(): boolean {
+    if (this.requestedEditorContent) {
+      prettier
+        .check(this.requestedEditorContent, {
+          parser: 'html',
+          plugins: [prettierPluginHtml],
+          bracketSameLine: true,
+        })
+        .then((value: boolean) => (this.isPrettified = value));
+    }
+    return this.isPrettified;
+  }
+
+  onViewChange(value: EditorView): void {
+    this.currentView = value;
+    if (this.originalCheckpointValue !== undefined && this.originalCheckpointValue !== null) {
+      if (this.currentView === 'raw') {
+        this.requestedEditorContent = this.originalCheckpointValue;
+        this.isPrettified = false;
+      } else {
+        this.prettify(this.originalCheckpointValue).then((prettifyResult) => {
+          this.requestedEditorContent = prettifyResult.text;
+          this.isPrettified = prettifyResult.isPrettified;
+        });
+      }
+    }
+  }
+
+  private setMonacoEditorHeight(): void {
+    const topComponentHeight = this.topComponent ? this.topComponent?.nativeElement.offsetHeight : 47;
+    const statusBarHeight = this.statusBar ? this.statusBar?.nativeElement.offsetHeight : 40;
+    this.monacoEditorHeight = this.containerHeight - topComponentHeight - statusBarHeight;
+    this.cdr.detectChanges();
+  }
+
+  private prettify(text: string): Promise<PrettifyResult> {
+    return new Promise<PrettifyResult>((resolve) => {
+      if (this.currentView === 'xml' && this.contentType === 'xml') {
+        prettier
+          .format(text, {
+            parser: 'html',
+            plugins: [prettierPluginHtml],
+            bracketSameLine: true,
+          })
+          .then((result: string): void => {
+            resolve({ text: result, isPrettified: true });
+          });
+      }
+      if (this.currentView === 'json' && this.contentType === 'json') {
+        const jsonFormatted: string = JSON.stringify(JSON.parse(text), null, this.INDENT_TWO_SPACES);
+        resolve({ text: jsonFormatted, isPrettified: true });
+      }
+      resolve({ text, isPrettified: false });
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  onActualEditorContentsChange(event: string): void {
+    console.log(`EditorComponent.onActualEditorContentsChange(): ${event.slice(0, 20)}`);
+    this.editorChangesSubject.next(event);
+    this.unsavedChanges = event !== this.originalCheckpointValue;
+  }
+
+  private setNewCheckpoint(value: string | null): void {
+    console.log('Got checkpoint value null');
+    this.originalCheckpointValue = value;
+    this.setContentType();
+    this.setAvailableViews();
+    if (value !== undefined && value !== null && value !== '') {
+      this.requestedEditorContent = value;
+      this.checkIfTextIsPretty();
+      if (this.showPrettifyOnLoad && this.isPrettifiable(this.contentType)) {
+        this.onViewChange(this.contentType);
+        return;
+      }
+      this.onViewChange('raw');
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  setContentType(): void {
+    if (this.originalCheckpointValue === undefined) {
+      throw new Error(
+        'EditorComponent.setContentType: expected that originalCheckpointValue was set because we call this method when a new checkpoint is selected',
+      );
+    }
+    if (this.originalCheckpointValue === null) {
+      this.contentType = 'raw';
+      return;
+    }
+    if (this.checkIfFileIsXml(this.originalCheckpointValue)) {
+      this.contentType = 'xml';
+      return;
+    }
+    try {
+      if (this.originalCheckpointValue && JSON.parse(this.originalCheckpointValue)) {
+        this.contentType = 'json';
+        return;
+      }
+    } catch {
+      //If error occurs, rawFile is not a json file
+    }
+    this.contentType = 'raw';
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  checkIfFileIsXml(value: string): boolean {
+    if (value) {
+      for (let index = 0; index < value.length; index++) {
+        if (value.charAt(index) === ' ' || value.charAt(index) === '\t') {
+          continue;
+        }
+        return value.charAt(index) === '<';
+      }
+    }
+    return false;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  getValue(): string {
+    return this.actualEditorContent ?? '';
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  setAvailableViews(): void {
+    const availableViews: EditorView[] = [...basicContentTypes];
+    if (!availableViews.includes(this.contentType)) {
+      availableViews.push(this.contentType);
+    }
+    this.availableViews = availableViews;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  isPrettifiable(value: EditorView): boolean {
+    return prettyContentTypes.includes(value as PrettyView);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  isBasicView(value: EditorView): boolean {
+    return basicContentTypes.includes(value as BasicView);
   }
 }
