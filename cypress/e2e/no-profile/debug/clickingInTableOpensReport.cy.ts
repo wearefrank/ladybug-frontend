5affee6d891a75a71760bfa9b3b5b3f1d0f542b0
Martijn Dirkse <mhdirkse@pm.me>
2025-10-23 11:09:37 +0200

    Fix test clickingInTableOpensReport

--- cypress/e2e/no-profile/debug/clickingInTableOpensReport.cy.ts
+++ cypress/e2e/no-profile/debug/clickingInTableOpensReport.cy.ts
@@ -18,10 +18,10 @@ describe('Clicking a report', () => {
   });
 
   it('Selecting report should show display', () => {
-    cy.get('[data-cy-debug-editor="buttons"]').should('not.be.visible');
-    cy.get('[data-cy-element-name="editor"]').should('not.exist');
+    cy.get('[data-cy-debug-editor="buttons"]').should('not.exist');
+    cy.get('[data-cy-element-name="checkpointEditor"]').should('not.exist');
     cy.getDebugTableRows().first().click();
     cy.get('[data-cy-debug-editor="buttons"]').should('be.visible');
-    cy.get('[data-cy-element-name="editor"]').should('be.visible');
+    cy.get('[data-cy-element-name="checkpointEditor"]').should('be.visible');
   });
 });
