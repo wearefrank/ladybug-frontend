2ebd13d14eaf0903ae9b45b423c283157255c0ab
Martijn Dirkse <martijn@wearefrank.nl>
2025-12-18 15:25:41 +0100

    Handle some edge cases of negative HTTP responses (#1153)
    * Handle some edge cases of negative HTTP responses
    
    * Better handle negative HTTP responses
    
    * Stick with any for now to avoid build errors

--- src/app/shared/classes/error-handling.service.ts
+++ src/app/shared/classes/error-handling.service.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { ToastService } from '../services/toast.service';
@@ -11,12 +9,15 @@ import { inject, Injectable } from '@angular/core';
 export class ErrorHandling {
   private toastService = inject(ToastService);
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   handleError(): (error: HttpErrorResponse) => Observable<any> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return (error: HttpErrorResponse): Observable<any> => {
+      console.warn(error);
       const message = error.error;
       if (error.status > 399 && error.status < 500) {
         this.toastService.showWarning(message);
-      } else if (message && message.includes('- detailed error message -')) {
+      } else if (message && typeof message === 'string' && message.includes('- detailed error message -')) {
         const errorMessageParts = message.split('- detailed error message -');
         this.toastService.showDanger(errorMessageParts[0], errorMessageParts[1]);
       } else {
