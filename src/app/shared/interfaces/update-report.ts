49748b472238add490604a40e4a99fb80291b8b6
Martijn Dirkse <mhdirkse@pm.me>
2026-01-13 11:05:54 +0100

    Send null values again for description, path and transformation

--- src/app/shared/interfaces/update-report.ts
+++ src/app/shared/interfaces/update-report.ts
@@ -1,11 +1,8 @@
 export interface UpdateReport {
   name?: string;
-  // Empty string is interpreted as request to clear the path
-  path?: string;
-  // Empty string is interpreted as request to clear the description
-  description?: string;
-  // Empty string is interpreted as request to clear the transformation
-  transformation?: string;
+  path?: string | null;
+  description?: string | null;
+  transformation?: string | null;
   variables?: Record<string, string>;
   stubStrategy?: string;
 }
